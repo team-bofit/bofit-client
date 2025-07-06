00d1175253608c9c7c7d4c8665837616230fcc0a
1jiwoo27 <chloey1227@naver.com>
2025-07-06 15:43:29 +0900

    chore: pnpm install

--- apps/client/src/pages/home/home-page.tsx
+++ apps/client/src/pages/home/home-page.tsx
@@ -5,9 +5,10 @@ const HomePage = () => {
   return (
     <div>
       <Navigation
-        leftIcon={<Icon name="arrow_left" />}
-        rightIcon={<Icon name="home" />}
-        title="커뮤니티"
+        leftIcon={<Icon name="arrow_left" color="white" />}
+        rightIcon={<Icon name="home" color="white" />}
+        title={<span style={{ color: 'white' }}>보허미님 반가워요!</span>}
+        backgroundColor="primary"
       />
     </div>
   );
