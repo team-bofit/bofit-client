3d7b0762fe55e19f3e7898c33a3ec90fe300ccfc
minjeoong <orange_mj@naver.com>
2025-07-06 20:03:05 +0900

    feat: 버튼 컴포넌트 구현

--- packages/bds-ui/src/components/button/button.tsx
+++ packages/bds-ui/src/components/button/button.tsx
@@ -1,5 +1,30 @@
-const Button = () => {
-  return <button>흥국생명 이름이 너무 길면 어떡하지.</button>;
+import { ButtonHTMLAttributes, ReactNode } from 'react';
+
+import { buttonSizes, buttonVariants } from './button.css';
+
+interface ButtonComponentProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: ReactNode;
+  disabled?: boolean;
+  variant?: 'primary' | 'error' | 'border';
+  size?: 'small' | 'medium' | 'large';
+}
+
+const Button = ({
+  children,
+  disabled = false,
+  size = 'medium',
+  variant = 'primary',
+  ...props
+}: ButtonComponentProps) => {
+  return (
+    <button
+      className={`${buttonVariants[variant]} ${buttonSizes[size]}`}
+      disabled={disabled}
+      {...props}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
