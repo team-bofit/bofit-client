b0087a71e5ed1f398c0afa4f9d234d6b8190b230
gwagjiug <kwjo0228@naver.com>
2025-07-06 17:12:33 +0900

    feat: 홈 페이지에 토스트 메시지 표시 기능 추가

--- apps/client/src/pages/home/home-page.tsx
+++ apps/client/src/pages/home/home-page.tsx
@@ -1,10 +1,20 @@
 import { Icon } from '@bds/icon';
+import { toasts } from '@bds/ui';
 
 const HomePage = () => {
+  const handleClick = () => {
+    toasts.show({
+      message: '3순위까지만 선택할 수 있어요',
+      duration: 5000,
+      icon: <Icon name="check" color="primary500" />,
+    });
+  };
+
   return (
     <div>
       home-page 배포 테스트입니다 하룰라라
       <Icon name="add" />
+      <button onClick={handleClick}>토스트 띄우기</button>
     </div>
   );
 };
