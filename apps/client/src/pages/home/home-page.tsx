e0d804ab2f9ae926ed9f8a39387d69c73ca3273c
jeonghoon11 <jih361@naver.com>
2025-07-06 17:52:55 +0900

    feat: home-page에서 Chip 컴포넌트 제거

--- apps/client/src/pages/home/home-page.tsx
+++ apps/client/src/pages/home/home-page.tsx
@@ -1,15 +1,10 @@
 import { Icon } from '@bds/icon';
-import { Chip } from '@bds/ui';
 
 const HomePage = () => {
   return (
     <div style={{ backgroundColor: 'white' }}>
       home-page 배포 테스트입니다 하룰라라
       <Icon name="add" />
-      <Chip label="테스트1" color="primary" size="rectangular" />
-      <Chip label="테스트2" color="gray" size="rectangular" />
-      <Chip label="테스트3" color="gray" size="rounded" outline={true} />
-      <Chip label="테스트4" color="primary" size="rounded" />
     </div>
   );
 };
