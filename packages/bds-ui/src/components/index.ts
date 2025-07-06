28d11826cc7ea420e9d741c7d866b30b71a2caef
jeonghoon11 <jih361@naver.com>
2025-07-06 23:08:49 +0900

    feat: Content 및 Title 컴포넌트 추가

--- packages/bds-ui/src/components/index.ts
+++ packages/bds-ui/src/components/index.ts
@@ -1,4 +1,6 @@
 export { default as Button } from './button/button';
 export { default as Chip } from './chip/chip';
+export { default as Contnet } from './content/content';
 export { default as Navigation } from './navigation/navigation';
 export { default as ThemeProvider } from './theme-provider';
+export { default as Title } from './title/title';
