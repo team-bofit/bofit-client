0813226fb6145838bad92ab7d469ce3762f59b0e
hansoojeongsj <tnwjd3758@gmail.com>
2025-07-06 20:44:14 +0900

    chore: homepage 복구

--- apps/client/src/pages/home/home-page.tsx
+++ apps/client/src/pages/home/home-page.tsx
@@ -1,15 +1,10 @@
 import { Icon } from '@bds/icon';
-import { Floating } from '@bds/ui';
 
 const HomePage = () => {
   return (
     <div>
       home-page 배포 테스트입니다 하룰라라
       <Icon name="add" />
-      <Floating
-        icon={<Icon name="edit" width={'100%'} height={'100%'} />}
-        state="default"
-      />
     </div>
   );
 };
