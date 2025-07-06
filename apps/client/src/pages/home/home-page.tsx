a52f28b2206a0e5b1cf8689d7a9ff320fb404ca7
1jiwoo27 <chloey1227@naver.com>
2025-07-06 18:35:05 +0900

    chore: test code 삭제

--- apps/client/src/pages/home/home-page.tsx
+++ apps/client/src/pages/home/home-page.tsx
@@ -1,14 +1,10 @@
 import { Icon } from '@bds/icon';
-import { Navigation } from '@bds/ui';
 
 const HomePage = () => {
   return (
     <div>
-      <Navigation
-        leftIcon={<Icon name="caret_left_lg" />}
-        rightIcon={<Icon name="home" />}
-        title="정보입력"
-      />
+      home-page 배포 테스트입니다 하룰라라
+      <Icon name="add" />
     </div>
   );
 };
