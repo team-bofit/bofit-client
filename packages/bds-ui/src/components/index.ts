0859ef9d4637db4a93f662ab407efa688bdbc9ad
hansoojeongsj <tnwjd3758@gmail.com>
2025-07-08 00:20:19 +0900

    Feat(bds): Floating 컴포넌트 구현 (#45)
    * feat: floating 컴포넌트 구현
    
    * chore: floating storybook 파일 세팅
    
    * chore: 스토리북 배포 오류 수정
    
    * chore: homepage 복구
    
    * feat: default에서 눌렀을 때 pressed로 수정
    
    * feat: recipe 사용하도록 css 파일 수정
    
    * chore: 스토리북 파일명 변경
    
    * feat: floating 컴포넌트 스토리북 추가

--- packages/bds-ui/src/components/index.ts
+++ packages/bds-ui/src/components/index.ts
@@ -2,6 +2,7 @@ export { default as Button } from './button/button';
 export { default as Chip } from './chip/chip';
 export { default as Content } from './content/content';
 export { default as Navigation } from './navigation/navigation';
+export { default as Floating } from './floating/floating';
 export { default as ThemeProvider } from './theme-provider';
 export { default as Title } from './title/title';
 export * from './toast/store/toast-store';
