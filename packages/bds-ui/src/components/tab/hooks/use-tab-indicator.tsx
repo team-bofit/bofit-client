c89963730e1cdbec136c4190cd1c3fc48b4e1b49
조혜린 <164613169+jogpfls@users.noreply.github.com>
2025-07-08 00:30:13 +0900

    Feat(bds): Tab 컴포넌트 구현 (#40)
    * chore: recipes 패키지 설치
    
    * feat: tab-provider 구현
    
    * feat: Tab 컴포넌트 기본 구조 구현
    
    * feat: Tab 컴파운드 패턴 구현
    
    * feat: Tab 포커싱 언더라인 구현
    
    * feat: index에 파일 추가
    
    * chore: Tab 컴포넌트 구현 테스트
    
    * chore: 테스트 삭제
    
    * feat: storybook 설정
    
    * fix: storybook title 수정
    
    * fix: 시맨틱 태그 수정
    
    * chore: 핸들러 함수 설정
    
    * fix: 조건을 변수로 분리
    
    * fix: devDependencies로 이동
    
    * fix: 컴파운드 패턴 구조 수정
    
    * fix: type 구조 수정
    
    * fix: 스타일 수정
    
    * chore: tab-provider 삭제 및 구조 변경
    
    * feat: pnpm update
    
    * fix: 스토리북 코드 수정
    
    * feat: indicator 애니매이션 적용
    
    * chore: 애니메이션 초기 값 설정
    
    * fix: storybook name 변경
    
    * fix: 애니메이션 초기값 0으로 수정
    
    * fix: lockfile update
    
    * chore: recipe 추가
    
    * fix: rem 단위 통일 수정
    
    * chore: 불필요한 주석 삭제
    
    ---------
    
    Co-authored-by: gwagjiug <kwjo0228@naver.com>

---
+++ packages/bds-ui/src/components/tab/hooks/use-tab-indicator.tsx
@@ -0,0 +1,22 @@
+import { useLayoutEffect, useRef, useState } from 'react';
+
+export const useTabIndicator = (initialValue: string) => {
+  const [selectedTab, setSelectedTab] = useState(initialValue);
+  const tabRefs = useRef<Record<string, HTMLLIElement | null>>({});
+  const [translateX, setTranslateX] = useState(0);
+
+  useLayoutEffect(() => {
+    const el = tabRefs.current[selectedTab];
+    if (el) {
+      const { offsetLeft, offsetWidth } = el;
+      const centerX = offsetLeft + offsetWidth / 2;
+      const fixedWidth = 50;
+      setTranslateX(centerX - fixedWidth / 2);
+    }
+  }, [selectedTab]);
+
+  return {
+    translateX,
+    contextValue: { selectedTab, setSelectedTab, tabRefs },
+  };
+};
---
+++ packages/bds-ui/src/components/tab/tab.tsx
@@ -0,0 +1,83 @@
+import { ReactNode } from 'react';
+
+import { TabContext, useTabContext } from './hooks/use-context';
+import { useTabIndicator } from './hooks/use-tab-indicator';
+
+import * as styles from './tab.css';
+
+interface ContainerProps {
+  children: ReactNode;
+  initialValue: string;
+}
+
+interface ListProps {
+  children: ReactNode;
+}
+
+interface ItemProps {
+  value: string;
+}
+
+interface PanelProps {
+  children: ReactNode;
+  tab: string;
+}
+
+const Container = ({ children, initialValue }: ContainerProps) => {
+  const { contextValue, translateX } = useTabIndicator(initialValue);
+
+  return (
+    <TabContext.Provider value={contextValue}>
+      <nav className={styles.tabContainer}>
+        <>{children}</>
+        <hr
+          className={styles.tabLine}
+          style={{
+            transform: `translateX(${translateX}px)`,
+          }}
+        />
+      </nav>
+    </TabContext.Provider>
+  );
+};
+
+const List = ({ children }: ListProps) => {
+  return <ul className={styles.tabList}>{children}</ul>;
+};
+
+const Item = ({ value }: ItemProps) => {
+  const { selectedTab, setSelectedTab, tabRefs } = useTabContext();
+  const isSelected = value === selectedTab;
+
+  const handleClick = () => {
+    setSelectedTab(value);
+  };
+
+  return (
+    <li
+      ref={(el) => {
+        tabRefs.current[value] = el;
+      }}
+      className={styles.tabItem({ selected: isSelected })}
+      onClick={handleClick}
+    >
+      <span>{value}</span>
+    </li>
+  );
+};
+
+const Panel = ({ children, tab }: PanelProps) => {
+  const { selectedTab } = useTabContext();
+  const isActive = selectedTab === tab;
+
+  return <>{isActive && children}</>;
+};
+
+const Tab = {
+  Container: Container,
+  List: List,
+  Item: Item,
+  Panel: Panel,
+};
+
+export default Tab;
