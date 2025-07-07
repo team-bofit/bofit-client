bd1c4b1cf5303abc9def0e4bc5a1b43cf21388b3
jeonghoon11 <jih361@naver.com>
2025-07-07 21:35:03 +0900

    refactor: ContentProps의 children 타입을 string으로 수정

--- packages/bds-ui/src/components/content/content.tsx
+++ packages/bds-ui/src/components/content/content.tsx
@@ -1,9 +1,7 @@
-import type { ReactNode } from 'react';
-
 import * as styles from './content.css';
 
 interface ContentProps {
-  children: ReactNode;
+  children: string;
 }
 
 const Content = ({ children }: ContentProps) => {
