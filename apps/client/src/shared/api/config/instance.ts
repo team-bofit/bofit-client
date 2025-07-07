f0ba399d32c9d583140502811aa5b9ca04eebdfa
minjeoong <orange_mj@naver.com>
2025-07-08 01:55:26 +0900

    feat: api instance에 토큰 서비스 세팅

--- apps/client/src/shared/api/config/instance.ts
+++ apps/client/src/shared/api/config/instance.ts
@@ -1,9 +1,33 @@
 import ky from '@toss/ky';
 
+import { authService } from '@shared/auth/services/auth-service.ts';
+import { tokenService } from '@shared/auth/services/token-service.ts';
+import { appConfig } from '@shared/configs/app-config.ts';
+
 export const api = ky.create({
-  prefixUrl: import.meta.env.VITE_BASE_URL,
+  prefixUrl: appConfig.api.baseUrl,
   headers: {
     'Content-Type': 'application/json',
   },
   retry: { limit: 0 },
+  hooks: {
+    beforeRequest: [
+      (request: Request) => {
+        const token = tokenService.getToken();
+        if (token) {
+          request.headers.set('Authorization', `Bearer ${token}`);
+        }
+      },
+    ],
+    afterResponse: [
+      async (response: Response) => {
+        if (response.status === 401) {
+          authService.logout();
+        }
+        /**
+         * @TODO 추후 리프레시 토큰 추가 함수 구현
+         */
+      },
+    ],
+  },
 });
