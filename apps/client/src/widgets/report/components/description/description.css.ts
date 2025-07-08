7e656607ada2405a44dc73e514084ec5198c87a2
jogpfls <joerin4177@gmail.com>
2025-07-08 18:17:21 +0900

    chore:  description 컴포넌트 삭제

--- apps/client/src/widgets/report/components/description/description.css.ts
+++
@@ -1,7 +0,0 @@
-import { themeVars } from '@bds/ui/styles';
-import { style } from '@vanilla-extract/css';
-
-export const description = style({
-  ...themeVars.fontStyles.body1_m_14,
-  color: themeVars.color.gray700,
-});
