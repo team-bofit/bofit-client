7607d9ad426b245acb927eae29de3ec49bd0682c
jogpfls <joerin4177@gmail.com>
2025-07-09 00:24:30 +0900

    feat: error 페이지 구현

---
+++ apps/client/src/shared/pages/error/error.css.ts
@@ -0,0 +1,41 @@
+import { themeVars } from '@bds/ui/styles';
+import { style } from '@vanilla-extract/css';
+
+export const errorContainer = style({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  width: '100%',
+  height: '100vh',
+});
+
+export const errorImage = style({
+  width: '100%',
+  maxWidth: '27rem',
+  marginRight: '1rem',
+});
+
+export const errorTitle = style({
+  ...themeVars.fontStyles.head2_b_24,
+  color: themeVars.color.primary500,
+  marginTop: '3.6rem',
+});
+
+export const errorDescription = style({
+  ...themeVars.fontStyles.body1_m_16,
+  color: themeVars.color.gray700,
+  whiteSpace: 'pre-wrap',
+  marginTop: '2.4rem',
+  textAlign: 'center',
+});
+
+export const errorButton = style({
+  ...themeVars.fontStyles.head2_b_18,
+  color: themeVars.color.primary500,
+  border: `1px solid ${themeVars.color.primary500}`,
+  width: '16.7rem',
+  height: '5.6rem',
+  borderRadius: '1.2rem',
+  marginTop: '4.0rem',
+});
---
+++ apps/client/src/shared/pages/error/error.tsx
@@ -0,0 +1,35 @@
+import { useNavigate } from 'react-router';
+
+import * as styles from './error.css';
+
+const ERROR_MESSAGES = {
+  title: '앗, 페이지를 찾을 수 없어요!',
+  description: [
+    '메인화면으로 돌아가거나,\n 주소가 맞는지 다시 한 번 확인해 주세요!',
+  ],
+  buttonText: '메인화면',
+};
+
+const Error = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/');
+  };
+  return (
+    <div className={styles.errorContainer}>
+      <img
+        className={styles.errorImage}
+        src="./error_3d_icon.webp"
+        alt="Error"
+      />
+      <p className={styles.errorTitle}>{ERROR_MESSAGES.title}</p>
+      <p className={styles.errorDescription}>{ERROR_MESSAGES.description}</p>
+      <button className={styles.errorButton} onClick={handleClick}>
+        {ERROR_MESSAGES.buttonText}
+      </button>
+    </div>
+  );
+};
+
+export default Error;
