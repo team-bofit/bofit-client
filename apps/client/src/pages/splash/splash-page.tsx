ff331b00e97910d2e3b1ed4a55643675fc0a03b5
1jiwoo27 <chloey1227@naver.com>
2025-07-08 17:40:52 +0900

    feat: splash 페이지 퍼블리싱

--- apps/client/src/pages/splash/splash-page.tsx
+++ apps/client/src/pages/splash/splash-page.tsx
@@ -1,9 +1,25 @@
+import { useEffect } from 'react';
 import { Icon } from '@bds/ui/icons';
+import { useNavigate } from 'react-router-dom';
+
+import { routePath } from '@shared/router/path';
+
+import * as styles from './splash-page.css';
 
 const SplashPage = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate(routePath.LOGIN);
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, [navigate]);
+
   return (
-    <div>
-      <Icon name="logo_3d" />
+    <div className={styles.container}>
+      <Icon name="logo_3d" width="14.0061rem" height="10.7rem" />
+      <Icon name="logotype_3d" width="15.9rem" height="5.4rem" />
     </div>
   );
 };
