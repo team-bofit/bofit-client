b1db2694df33179be71327ad308c3e000a82b4ef
gwagjiug <kwjo0228@naver.com>
2025-07-08 22:56:54 +0900

    feat: modal 스토리북 임시 삭제

--- apps/client/src/pages/home/home-page.tsx
+++ apps/client/src/pages/home/home-page.tsx
@@ -1,5 +1,31 @@
+import { Button, Modal, useModal } from '@bds/ui';
+
 const HomePage = () => {
-  return <div style={{ backgroundColor: 'white' }}></div>;
+  const { openModal, closeModal } = useModal();
+
+  const handleOpenModal = () => {
+    openModal(
+      <Modal>
+        <Modal.Title>이 댓글을 삭제할까요?</Modal.Title>
+        <Modal.Content>삭제한 댓글은 복원되지 않습니다.</Modal.Content>
+        <Modal.Actions>
+          <Button onClick={closeModal}>취소</Button>
+          <Button variant="error" onClick={closeModal}>
+            확인
+          </Button>
+        </Modal.Actions>
+      </Modal>,
+      {
+        closeOnBackdropClick: true,
+        backdrop: true,
+      },
+    );
+  };
+  return (
+    <div style={{ backgroundColor: 'white' }}>
+      <Button onClick={handleOpenModal}>모달 열기</Button>
+    </div>
+  );
 };
 
 export default HomePage;
