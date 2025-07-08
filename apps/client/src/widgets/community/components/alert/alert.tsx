08203ecb1f388e22462a213493911a626de14022
jeonghoon11 <jih361@naver.com>
2025-07-08 14:54:38 +0900

    fix: ALERT_CONTENT 상수 이름 변경 및 내용 구조 수정

--- apps/client/src/widgets/community/components/alert/alert.tsx
+++ apps/client/src/widgets/community/components/alert/alert.tsx
@@ -1,7 +1,7 @@
 import { Content } from '@bds/ui';
 import { Icon } from '@bds/ui/icons';
 
-import { ALERT_CONTENT } from './constant/ALERT_CONTENT';
+import { ALERT_CONTENT_BODY } from './constant/alert_content';
 
 import * as styles from './alert.css';
 
@@ -10,9 +10,9 @@ const Alert = () => {
     <div className={styles.container}>
       <div className={styles.alertHeader}>
         <Icon name="info" color="primary500" width="2.4rem" height="2.4rem" />
-        <p className={styles.headerContent}>{ALERT_CONTENT.HEADER_CONTENT}</p>
+        <p className={styles.headerContent}>{ALERT_CONTENT_BODY.HEADER}</p>
       </div>
-      <Content text={ALERT_CONTENT.BODY_CONTENT} length="sm" />
+      <Content text={ALERT_CONTENT_BODY.BODY} length="sm" />
     </div>
   );
 };
