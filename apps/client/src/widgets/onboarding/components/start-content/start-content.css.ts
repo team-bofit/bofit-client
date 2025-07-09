5f2fb8ec8b857a24de2fa590b518361ea6236d8c
hansoojeongsj <tnwjd3758@gmail.com>
2025-07-10 03:51:12 +0900

    feat: 정보입력 시작 페이지 구현

---
+++ apps/client/src/widgets/onboarding/components/start-content/start-content.css.ts
@@ -0,0 +1,40 @@
+import { themeVars } from '@bds/ui/styles';
+import { style } from '@vanilla-extract/css';
+
+export const container = style({
+  display: 'flex',
+  flexDirection: 'column',
+  padding: 'clamp(2.4rem, 12vw, 6.4rem) 1.6rem 11.9rem',
+});
+
+export const title = style({
+  marginBottom: '0.8rem',
+  whiteSpace: 'pre-wrap',
+  color: themeVars.color.gray900,
+  ...themeVars.fontStyles.head_eb_24,
+});
+
+export const message = style({
+  marginBottom: 'clamp(2.4rem, 16vw, 7.6rem)',
+  whiteSpace: 'pre-wrap',
+  color: themeVars.color.gray700,
+  ...themeVars.fontStyles.body2_r_14,
+});
+
+export const image = style({
+  width: '28rem',
+  margin: '0 auto',
+});
+
+export const button = style({
+  position: 'fixed',
+  bottom: 0,
+
+  maxWidth: '43rem',
+  width: '100vw',
+  display: 'flex',
+
+  flexDirection: 'column',
+  padding: '0 1.6rem 2.4rem',
+  gap: '1.6rem',
+});
--- apps/client/src/widgets/onboarding/components/start-content/start-content.tsx
+++ apps/client/src/widgets/onboarding/components/start-content/start-content.tsx
@@ -1,15 +1,29 @@
-import { Button } from '@bds/ui';
+import { Button, TextButton } from '@bds/ui';
 
-const StartContent = () => {
+import * as styles from '@widgets/onboarding/components/start-content/start-content.css';
+
+const StartContent = ({ userName }: { userName: string }) => {
+  const TITLE_TEXT = `${userName}님을 위한 맞춤 추천,\n지금 시작할게요!`;
+  const MESSAGE_TEXT = `꼭 맞는 보험을 찾을 수 있도록, \n간단히 몇 개만 여쭤볼게요.`;
+
   return (
     <>
-      <p>00님을 위한 맞춤 추천, 지금 시작할게요!</p>
-      <p>꼭 맞는 보험을 찾을 수 있도록, 간단히 몇 개만 여쭤볼게요.</p>
+      <section className={styles.container}>
+        <p className={styles.title}>{TITLE_TEXT}</p>
+        <p className={styles.message}>{MESSAGE_TEXT}</p>
 
-      <img src="./glass_icon_info_check.webp" alt="Onboarding Start" />
-      <Button variant="primary" size="lg">
-        정보 입력 시작하기
-      </Button>
+        <img
+          className={styles.image}
+          src="./glass_icon_info_check.webp"
+          alt="Onboarding Start"
+        />
+      </section>
+      <div className={styles.button}>
+        <Button variant="primary" size="lg">
+          정보 입력 시작하기
+        </Button>
+        <TextButton color={'black'}>나중에 추천받을래요</TextButton>
+      </div>
     </>
   );
 };
