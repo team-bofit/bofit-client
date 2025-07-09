c5765e37cff9492637bd19c360871b5000aa2be9
hansoojeongsj <tnwjd3758@gmail.com>
2025-07-10 01:21:19 +0900

    chore: webp 파일 추가

--- apps/client/src/widgets/onboarding/components/start-content/start-content.tsx
+++ apps/client/src/widgets/onboarding/components/start-content/start-content.tsx
@@ -1,5 +1,17 @@
+import { Button } from '@bds/ui';
+
 const StartContent = () => {
-  return <div>start-content</div>;
+  return (
+    <>
+      <p>00님을 위한 맞춤 추천, 지금 시작할게요!</p>
+      <p>꼭 맞는 보험을 찾을 수 있도록, 간단히 몇 개만 여쭤볼게요.</p>
+
+      <img src="./glass_icon_info_check.webp" alt="Onboarding Start" />
+      <Button variant="primary" size="lg">
+        정보 입력 시작하기
+      </Button>
+    </>
+  );
 };
 
 export default StartContent;
