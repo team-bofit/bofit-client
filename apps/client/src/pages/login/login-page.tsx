0ae783e2a8368d1035c47ce07bb674620d8dd5d2
minjeoong <orange_mj@naver.com>
2025-07-09 16:28:03 +0900

    feat: 에러 테스트 코드 제거

--- apps/client/src/pages/login/login-page.tsx
+++ apps/client/src/pages/login/login-page.tsx
@@ -1,13 +1,5 @@
 const LoginPage = () => {
-  const handleError = () => {
-    throw new Error('로그인 페이지에서 오류 발생');
-  };
-  return (
-    <div>
-      LoginPage
-      <button onClick={handleError}>하하하</button>
-    </div>
-  );
+  return <div>LoginPage</div>;
 };
 
 export default LoginPage;
