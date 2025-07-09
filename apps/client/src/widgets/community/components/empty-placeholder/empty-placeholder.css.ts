d6d3a3569169fc346cb8a7657586f2059c2dd768
JeongHoon <113702672+jeonghoon11@users.noreply.github.com>
2025-07-09 13:03:52 +0900

    Feat(client): 커뮤니티 empty 화면 퍼블리싱 및 floating컴포넌트 수정 (#88)
    * feat: 커뮤니티 페이지 및 빈 플레이스홀더 컴포넌트 추가
    
    * feat: EmptyPlaceholder 컴포넌트에 content prop 추가 및 CommunityPage에서 사용
    
    * feat: EmptyPlaceholder 컴포넌트에서 Icon의 color 속성 제거 및 empty-content 상수명 변경
    
    * fix: empty-content 상수명에서 언더스코어를 하이픈으로 변경
    
    * fix: Floating 컴포넌트의 onClick 주석을 TODO로 변경

---
+++ apps/client/src/widgets/community/components/empty-placeholder/empty-placeholder.css.ts
@@ -0,0 +1,19 @@
+import { themeVars } from '@bds/ui/styles';
+import { style } from '@vanilla-extract/css';
+
+export const container = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '1.8rem',
+});
+
+export const wrapper = style({
+  padding: '0 10.8rem 0 13.2rem',
+});
+
+export const content = style({
+  whiteSpace: 'pre-wrap',
+  color: themeVars.color.gray300,
+  ...themeVars.fontStyles.title_sb_16,
+  textAlign: 'center',
+});
