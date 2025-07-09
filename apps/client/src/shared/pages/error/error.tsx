ef48db526f2e7a1238f34791c7889e3b3743979a
jogpfls <joerin4177@gmail.com>
2025-07-09 15:58:35 +0900

    fix: 상수 대문자로 수정

--- apps/client/src/shared/pages/error/error.tsx
+++ apps/client/src/shared/pages/error/error.tsx
@@ -3,11 +3,11 @@ import { useNavigate } from 'react-router';
 import * as styles from './error.css';
 
 const ERROR_MESSAGES = {
-  title: '앗, 페이지를 찾을 수 없어요!',
-  description: [
+  TITLE: '앗, 페이지를 찾을 수 없어요!',
+  DESCRIPTION: [
     '메인화면으로 돌아가거나,\n 주소가 맞는지 다시 한 번 확인해 주세요!',
   ],
-  buttonText: '메인화면',
+  BUTTON_TEXT: '메인화면',
 };
 
 const ErrorPage = () => {
@@ -16,6 +16,7 @@ const ErrorPage = () => {
   const handleClick = () => {
     navigate('/');
   };
+
   return (
     <div className={styles.errorContainer}>
       <img
@@ -23,10 +24,10 @@ const ErrorPage = () => {
         src="./error_3d_icon.webp"
         alt="Error"
       />
-      <p className={styles.errorTitle}>{ERROR_MESSAGES.title}</p>
-      <p className={styles.errorDescription}>{ERROR_MESSAGES.description}</p>
+      <p className={styles.errorTitle}>{ERROR_MESSAGES.TITLE}</p>
+      <p className={styles.errorDescription}>{ERROR_MESSAGES.DESCRIPTION}</p>
       <button className={styles.errorButton} onClick={handleClick}>
-        {ERROR_MESSAGES.buttonText}
+        {ERROR_MESSAGES.BUTTON_TEXT}
       </button>
     </div>
   );
