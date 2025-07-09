811f6f167168e27a661b717a8ff27494e9930ef7
minjeoong <orange_mj@naver.com>
2025-07-10 03:29:18 +0900

    feat: tip components 구현

--- apps/client/src/pages/home/home-page.tsx
+++ apps/client/src/pages/home/home-page.tsx
@@ -1,3 +1,5 @@
+import { Indicator } from '@bds/ui';
+
 import Tip from '@widgets/home/components/tip/tip.tsx';
 
 const HomePage = () => {
@@ -7,8 +9,13 @@ const HomePage = () => {
       <Tip
         title="보험 상령일이란?"
         contents="생일에 6개월을 더한 날로, 보험료 인상 기준이 돼요."
+      />
+      <Tip
+        title="보험 상령일이란?"
+        contents="생일에 6개월을 더한 날로, 보험료 인상 기준이 돼요."
         variant="gray"
       />
+      <Indicator current={0} total={2} />
     </>
   );
 };
--- apps/client/src/widgets/home/components/tip/tip.tsx
+++ apps/client/src/widgets/home/components/tip/tip.tsx
@@ -9,6 +9,14 @@ interface TipProps extends HTMLAttributes<HTMLDivElement> {
   onClick?: VoidFunction;
 }
 
+/**
+ * Tip 컴포넌트는 홈화면에서 보험 관련 정보를 제공하는 팁을 표시합니다.
+ * @param title 보험 tip 제목
+ * @param contents 보험 tip 내용
+ * @param variant 팁의 색상 변형 (기본값: 'green')
+ * @param onClick 팁 클릭 시 실행되는 함수
+ * @param props 기타 HTML 속성
+ */
 const Tip = ({
   title,
   contents,
