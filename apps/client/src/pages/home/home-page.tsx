47762177993d37b17125527e3eebefef488bb6ac
minjeoong <orange_mj@naver.com>
2025-07-10 19:33:39 +0900

    feat: 테스트 코드 제거

--- apps/client/src/pages/home/home-page.tsx
+++ apps/client/src/pages/home/home-page.tsx
@@ -1,23 +1,5 @@
-import { Indicator } from '@bds/ui';
-
-import Tip from '@widgets/home/components/tip/tip.tsx';
-
 const HomePage = () => {
-  return (
-    <>
-      HomePage
-      <Tip
-        title="보험 상령일이란?"
-        contents="생일에 6개월을 더한 날로, 보험료 인상 기준이 돼요."
-      />
-      <Tip
-        title="보험 상령일이란?"
-        contents="생일에 6개월을 더한 날로, 보험료 인상 기준이 돼요."
-        variant="gray"
-      />
-      <Indicator current={0} total={2} />
-    </>
-  );
+  return <>HomePage</>;
 };
 
 export default HomePage;
