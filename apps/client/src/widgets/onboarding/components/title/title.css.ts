ea94d65078e08675481eb89ed2c3ed02574a3c62
hansoojeongsj <tnwjd3758@gmail.com>
2025-07-10 21:46:12 +0900

    feat: 온보딩 title 컴포넌트

---
+++ apps/client/src/widgets/onboarding/components/title/title.css.ts
@@ -0,0 +1,26 @@
+import { themeVars } from '@bds/ui/styles';
+import { style } from '@vanilla-extract/css';
+
+export const section = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '0.8rem',
+});
+
+export const title = style({
+  color: themeVars.color.gray400,
+  ...themeVars.fontStyles.title_sb_14,
+  whiteSpace: 'pre-wrap',
+});
+
+export const description = style({
+  color: themeVars.color.gray900,
+  ...themeVars.fontStyles.head_eb_24,
+  whiteSpace: 'pre-wrap',
+});
+
+export const caption = style({
+  color: themeVars.color.gray600,
+  ...themeVars.fontStyles.body1_m_14,
+  whiteSpace: 'pre-wrap',
+});
--- apps/client/src/widgets/onboarding/components/title/title.tsx
+++ apps/client/src/widgets/onboarding/components/title/title.tsx
@@ -1,9 +1,18 @@
-const title = () => {
+import * as styles from './title.css';
+interface TitleProps {
+  title: string;
+  description: string;
+  caption?: string;
+}
+
+const Title = ({ title, description, caption }: TitleProps) => {
   return (
-    <>
-      <p>title</p>
-    </>
+    <section className={styles.section}>
+      <h2 className={styles.title}>{title}</h2>
+      <p className={styles.description}>{description}</p>
+      {caption && <small className={styles.caption}>{caption}</small>}
+    </section>
   );
 };
 
-export default title;
+export default Title;
