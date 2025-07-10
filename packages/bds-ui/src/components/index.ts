439dc1243499a57352a4252a121045e5f54cdb07
jeonghoon11 <jih361@naver.com>
2025-07-10 12:22:48 +0900

    feat: profile 아이콘 추가

--- packages/bds-ui/src/components/index.ts
+++ packages/bds-ui/src/components/index.ts
@@ -1,3 +1,4 @@
+export { default as Avatar } from './avator/avatar';
 export { default as Button } from './button/button';
 export { default as Chip } from './chip/chip';
 export { default as Content } from './content/content';
