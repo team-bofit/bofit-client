aa7dfcaf914539659f17f26ec617b295f5bd5231
gwagjiug <kwjo0228@naver.com>
2025-07-11 00:25:36 +0900

    feat: subtitle 컴포넌트 분리

---
+++ apps/client/src/widgets/login/components/login-slide/sub-title.css.ts
@@ -0,0 +1,8 @@
+import { themeVars } from '@bds/ui/styles';
+import { style } from '@vanilla-extract/css';
+
+export const descriptionText = style({
+  width: '100%',
+  ...themeVars.fontStyles.body1_m_16,
+  color: themeVars.color.gray600,
+});
