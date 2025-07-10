a72921ecd0b8ad034aab57120f48ab781f3755ed
hansoojeongsj <tnwjd3758@gmail.com>
2025-07-10 19:35:39 +0900

    feat: interface 추가

--- apps/client/src/widgets/onboarding/components/start-content/start-content.tsx
+++ apps/client/src/widgets/onboarding/components/start-content/start-content.tsx
@@ -2,7 +2,11 @@ import { Button, TextButton } from '@bds/ui';
 
 import * as styles from '@widgets/onboarding/components/start-content/start-content.css';
 
-const StartContent = ({ userName }: { userName: string }) => {
+interface StartContentProps {
+  userName: string;
+}
+
+const StartContent = ({ userName }: StartContentProps) => {
   const TITLE_TEXT = `${userName}님을 위한 맞춤 추천,\n지금 시작할게요!`;
   const MESSAGE_TEXT = `꼭 맞는 보험을 찾을 수 있도록, \n간단히 몇 개만 여쭤볼게요.`;
 
