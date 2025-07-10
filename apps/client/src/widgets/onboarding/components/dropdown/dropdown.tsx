ecc0a00cb83247149e31a3f4ddc4a67f46b6b0e9
1jiwoo27 <101466143+1jiwoo27@users.noreply.github.com>
2025-07-10 23:48:59 +0900

    Feat(client): Dropdown 컴포넌트 구현 (#118)
    * feat: class 컴포넌트 위치 변경
    
    * feat: gitkeep 삭제
    
    * feat: dropdown 구현중
    
    * feat: dropdown 구현
    
    * chore: home-page 내용 삭제
    
    * chore: 로그인 페이지 변경 사항 삭제
    
    * feat: icon 회전 구현
    
    * chore: userSelect none 추가
    
    * chore: zIndex 추가
    
    * feat: selected item은 hover 적용 안 되도록 수정
    
    * chore: padding 값 수정
    
    * feat: dropdown position 설정
    
    * feat: icon rotate 값 지정
    
    * chore: placeholder 상수화
    
    * chore: handler 이름 변경
    
    * chore: icon 방향 수정
    
    * feat: setter 대신 handler 함수 주입
    
    * feat: optionItem 컴포넌트 분리
    
    * chore: clssName 수정
    
    ---------
    
    Co-authored-by: gwagjiug <kwjo0228@naver.com>

---
+++ apps/client/src/widgets/onboarding/components/dropdown/dropdown.tsx
@@ -0,0 +1,71 @@
+import { useState } from 'react';
+import { Icon } from '@bds/ui/icons';
+
+import OptionItem from './option-item';
+
+import * as styles from './dropdown.css';
+
+const OPTIONS = [
+  '사무',
+  '교육/전문',
+  '서비스',
+  '운송/배달',
+  '생산/기술',
+  '예술/프리랜서',
+  '학생/구직자',
+  '해당없음(기타)',
+];
+
+const DEFAULT_PLACEHOLDER = '직업을 선택해주세요.';
+
+const DropDown = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selected, setSelected] = useState<string | null>(null);
+
+  const handleOptionClick = (option: string) => {
+    setSelected(option);
+    setIsOpen(false);
+  };
+
+  const handleToggleDropdown = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  return (
+    <div className={styles.dropdownWrapper}>
+      <div
+        className={
+          !isOpen ? styles.dropdownContainer : styles.dropdownContainerOpen
+        }
+        onClick={handleToggleDropdown}
+      >
+        <div className={styles.dropdownPlaceholder}>
+          {selected || DEFAULT_PLACEHOLDER}
+        </div>
+        <Icon
+          name="caret_up_md"
+          color="gray800"
+          width="2.4rem"
+          height="2.4rem"
+          className={styles.icon}
+          rotate={isOpen ? undefined : 180}
+        />
+      </div>
+
+      {isOpen && (
+        <ul className={styles.dropdownList}>
+          {OPTIONS.map((option) => (
+            <OptionItem
+              key={option}
+              option={option}
+              isSelected={option === selected}
+              onSelect={handleOptionClick}
+            />
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default DropDown;
