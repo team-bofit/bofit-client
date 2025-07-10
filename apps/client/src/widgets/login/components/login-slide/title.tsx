82498371d786af75e1f49504ae7308e64f9684b7
gwagjiug <kwjo0228@naver.com>
2025-07-11 00:25:47 +0900

    feat: title 컴포넌트 추상화

---
+++ apps/client/src/widgets/login/components/login-slide/title.tsx
@@ -0,0 +1,13 @@
+import * as styles from './title.css';
+
+const Title = () => {
+  return (
+    <h2 className={styles.headerText}>
+      보험, 이제는
+      <br />
+      스스로 이해하고 선택해요
+    </h2>
+  );
+};
+
+export default Title;
