c5de9e30727aca968b00c98d0da51c87e8f2ab61
minjeoong <orange_mj@naver.com>
2025-07-11 18:03:36 +0900

    feat: feature-section 구성

--- apps/client/src/widgets/home/components/info-section/info-section.css.ts
+++ apps/client/src/widgets/home/components/info-section/info-section.css.ts
@@ -1,3 +1,12 @@
+import { themeVars } from '@bds/ui/styles';
 import { style } from '@vanilla-extract/css';
 
 export const infoSection = style({});
+export const titleSection = style({});
+export const subTitle = style({
+  ...themeVars.fontStyles.title_sb_14,
+});
+export const title = style({});
+
+export const chipList = style({});
+export const homeChipList = style({});
--- apps/client/src/widgets/home/components/info-section/info-section.tsx
+++ apps/client/src/widgets/home/components/info-section/info-section.tsx
@@ -1,5 +1,14 @@
 import * as styles from './info-section.css.ts';
 
 export const InfoSection = () => {
-  return <section className={styles.infoSection}></section>;
+  return (
+    <section className={styles.infoSection}>
+      <div className={styles.titleSection}>
+        <p className={styles.subTitle}>OO님께 딱 맞는 보험이에요</p>
+        <p className={styles.title}>OO보험사의 \n OO보험</p>
+        <div className={styles.chipList}></div>
+      </div>
+      <div className={styles.homeChipList}></div>
+    </section>
+  );
 };
