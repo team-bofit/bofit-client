4b3ec92bcd6a0ba484f318aa7c3725a2af0acb8e
hansoojeongsj <tnwjd3758@gmail.com>
2025-07-11 15:26:26 +0900

    chore: 상수를 컴포넌트 외부로 분리

--- apps/client/src/widgets/onboarding/components/start-content/start-content.tsx
+++ apps/client/src/widgets/onboarding/components/start-content/start-content.tsx
@@ -5,16 +5,19 @@ import * as styles from '@widgets/onboarding/components/start-content/start-cont
 interface StartContentProps {
   userName: string;
 }
+const TEXT = {
+  TITLE: '님을 위한 맞춤 추천,\n지금 시작할게요!',
+  MESSAGE: '꼭 맞는 보험을 찾을 수 있도록, \n간단히 몇 개만 여쭤볼게요.',
+};
 
 const StartContent = ({ userName }: StartContentProps) => {
-  const TITLE_TEXT = `${userName}님을 위한 맞춤 추천,\n지금 시작할게요!`;
-  const MESSAGE_TEXT = `꼭 맞는 보험을 찾을 수 있도록, \n간단히 몇 개만 여쭤볼게요.`;
+  const titleText = `${userName}${TEXT.TITLE}`;
 
   return (
     <>
       <section className={styles.container}>
-        <p className={styles.title}>{TITLE_TEXT}</p>
-        <p className={styles.message}>{MESSAGE_TEXT}</p>
+        <p className={styles.title}>{titleText}</p>
+        <p className={styles.message}>{TEXT.MESSAGE}</p>
         <div className={styles.imageContainer}>
           <img
             className={styles.image}
