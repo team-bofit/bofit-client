385323391301d64859e71ab1296abd3e8cbcd322
jogpfls <joerin4177@gmail.com>
2025-07-11 19:16:53 +0900

    feat: 심장 질환 컴포넌트 구현

---
+++ apps/client/src/widgets/report/components/keunbyeong/shimjang.tsx
@@ -0,0 +1,148 @@
+import { Alert } from '@bds/ui';
+
+import { Accordion } from '../accordion/accordion';
+import Graph from '../graph/graph';
+import Info from '../info/info';
+import Title from '../title/title';
+
+import * as styles from './shimjang.css';
+
+const shimjangData = {
+  coverageStatus: '강력',
+  additional_info:
+    '심근경색보다 허혈성심장질환(협심증, 급성심근경색 포함)이 더 넓은 보장이에요. 확대심장질환과 심부전, 부정맥 특약도 고려해볼 수 있어요.',
+  acuteMyocardialInfarction: {
+    diagnosis: {
+      productCoverage: 0,
+      averageCoverage: 2000,
+    },
+    injury: {
+      productCoverage: 2000,
+      averageCoverage: 1000,
+    },
+  },
+  ischemic: {
+    diagnosis: {
+      productCoverage: 1000,
+      averageCoverage: 0,
+    },
+    injury: {
+      productCoverage: 0,
+      averageCoverage: 0,
+    },
+  },
+  extended: {
+    diagnosis: {
+      productCoverage: 0,
+      averageCoverage: 0,
+    },
+    injury: {
+      productCoverage: 0,
+      averageCoverage: 0,
+    },
+  },
+  arrhythmia: {
+    diagnosis: {
+      productCoverage: 0,
+      averageCoverage: 1000,
+    },
+    injury: {
+      productCoverage: 0,
+      averageCoverage: 2000,
+    },
+  },
+} as const;
+
+const SECTION = [
+  { title: '급성 심근경색', key: 'acuteMyocardialInfarction' },
+  { title: '허혈성 심장질환', key: 'ischemic' },
+  { title: '확대 심장질환', key: 'extended' },
+  { title: '부정맥, 심부전', key: 'arrhythmia' },
+] as const;
+
+const Noehyeolgwan = () => {
+  return (
+    <div>
+      <Accordion>
+        <Accordion.Header type="강력"> 심장질환</Accordion.Header>
+        <Accordion.Panel>
+          <Info
+            description={shimjangData.additional_info}
+            size="sm"
+            iconSize="1.6rem"
+          />
+          <div className={styles.allgraphContainer}>
+            {SECTION.map(({ title, key }) => {
+              const diagnosisCoverage =
+                shimjangData[key].diagnosis.productCoverage;
+              const injuryCoverage = shimjangData[key].injury.productCoverage;
+              const showAllAlert =
+                diagnosisCoverage === 0 && injuryCoverage === 0;
+              return (
+                <div key={key} className={styles.graphContainer}>
+                  <Title category="subCategory" title={title} />
+                  <div className={styles.graphContentsContainer}>
+                    {showAllAlert ? (
+                      <Alert
+                        type="additional"
+                        iconName="info_warning"
+                        iconSize="2rem"
+                        alertHeader="참고하세요!"
+                        alertContents="은 이 보험에 포함되지 않아요."
+                        highlight={title}
+                      />
+                    ) : (
+                      <>
+                        {diagnosisCoverage !== 0 ? (
+                          <Graph
+                            detailItem="진단비"
+                            average={
+                              shimjangData[key].diagnosis.averageCoverage
+                            }
+                            current={diagnosisCoverage}
+                          />
+                        ) : (
+                          <div className={styles.alertDiagnosisContainer}>
+                            <Alert
+                              type="additional"
+                              iconName="info_warning"
+                              iconSize="2rem"
+                              alertHeader="참고하세요!"
+                              alertContents="는 이 보험에 포함되지 않아요."
+                              highlight={`진단비`}
+                            />
+                          </div>
+                        )}
+
+                        {injuryCoverage !== 0 ? (
+                          <Graph
+                            detailItem="수술비"
+                            average={shimjangData[key].injury.averageCoverage}
+                            current={injuryCoverage}
+                          />
+                        ) : (
+                          <div className={styles.alertInjuryContainer}>
+                            <Alert
+                              type="additional"
+                              iconName="info_warning"
+                              iconSize="2rem"
+                              alertHeader="참고하세요!"
+                              alertContents="는 이 보험에 포함되지 않아요."
+                              highlight={`수술비`}
+                            />
+                          </div>
+                        )}
+                      </>
+                    )}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </Accordion.Panel>
+      </Accordion>
+    </div>
+  );
+};
+
+export default Noehyeolgwan;
