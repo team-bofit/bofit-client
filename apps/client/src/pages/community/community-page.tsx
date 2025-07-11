3adc695475e1bc8f012e508fa181c235f4b937d6
jeonghoon11 <jih361@naver.com>
2025-07-11 16:20:35 +0900

    fix: 테스트용 컴포넌트 불러옴 수정

--- apps/client/src/pages/community/community-page.tsx
+++ apps/client/src/pages/community/community-page.tsx
@@ -3,7 +3,6 @@ import { Alert } from '@bds/ui';
 import { Icon } from '@bds/ui/icons';
 
 import EmptyPlaceholder from '@widgets/community/components/empty-placeholder/empty-placeholder';
-import UserComment from '@widgets/community/components/user-comment/user-comment';
 import { ALERT_CONTENT_BODY } from '@widgets/community/constant/alert_content';
 import { EMPTY_POST } from '@widgets/community/constant/empty-content';
 
@@ -20,9 +19,6 @@ const CommunityPage = () => {
         alertContents={ALERT_CONTENT_BODY.BODY}
         alertHeader={ALERT_CONTENT_BODY.HEADER}
       />
-      <br />
-      <UserComment comment="저도요" nickName="닉네임" timestamp="10" />
-      <br />
       <div className={styles.emptyPlaceholder}>
         <EmptyPlaceholder content={EMPTY_POST} />
       </div>
