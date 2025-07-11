b43f2a257821e6ef6ae323500a8efcf0b45e7a89
jeonghoon11 <jih361@naver.com>
2025-07-11 18:45:45 +0900

    feat: 댓글 작성 컴포넌트 구현

---
+++ apps/client/src/widgets/community/components/comment-box/comment-box.tsx
@@ -0,0 +1,42 @@
+import { ChangeEvent } from 'react';
+import { Input } from '@bds/ui';
+import { Icon } from '@bds/ui/icons';
+
+import { PLACEHOLDER } from '@widgets/community/constant/INPUT_PLACEHOLDER';
+
+import * as styles from './comment-box.css';
+
+interface CommentBoxProps {
+  value: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  errorState?: boolean;
+  onSubmit?: () => void;
+}
+
+const CommentBox = ({
+  value,
+  onChange,
+  errorState,
+  onSubmit,
+}: CommentBoxProps) => {
+  return (
+    <div className={styles.container}>
+      <Input
+        value={value}
+        onChange={onChange}
+        bgColor="white"
+        placeholder={PLACEHOLDER.COMMENT}
+        errorState={errorState}
+      />
+      <Icon
+        name="btn_comment"
+        width="4.8rem"
+        height="4.8rem"
+        onClick={onSubmit}
+        //  TODO: onSubmit 댓글 추가
+      />
+    </div>
+  );
+};
+
+export default CommentBox;
