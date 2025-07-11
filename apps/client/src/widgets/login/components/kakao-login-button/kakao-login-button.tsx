6fad6330927347931710a79a4e4388c8f4e885e2
gwagjiug <kwjo0228@naver.com>
2025-07-11 19:57:27 +0900

    feat: kakao-login button 및 modal 구현

--- apps/client/src/widgets/login/components/kakao-login-button/kakao-login-button.tsx
+++ apps/client/src/widgets/login/components/kakao-login-button/kakao-login-button.tsx
@@ -1,13 +1,23 @@
+import { useModal } from '@bds/ui';
 import { Icon } from '@bds/ui/icons';
+
+import KaKaoLoginModal from './kakao-login-modal';
 
 import * as styles from './kakao-login-button.css';
 
 const KakaoLoginButton = () => {
+  const { openModal } = useModal();
+
+  const handleOpenModal = () => {
+    openModal(<KaKaoLoginModal />);
+  };
+
   return (
-    <div className={styles.container}>
+    <div className={styles.container} onClick={handleOpenModal}>
       <Icon name="kakaotalk" width="2.7rem" height="2.7rem" />
       <p className={styles.text}>카카오톡으로 시작하기</p>
     </div>
   );
 };
+
 export default KakaoLoginButton;
