01dc96a000b6a1b88ef871b210edcb9fde2d27d5
gwagjiug <kwjo0228@naver.com>
2025-07-11 16:47:10 +0900

    feat: login-slide 퍼블리싱

--- apps/client/src/widgets/login/components/login-slide/login-slide.css.ts
+++ apps/client/src/widgets/login/components/login-slide/login-slide.css.ts
@@ -1,57 +1,50 @@
-import { themeVars } from '@bds/ui/styles';
 import { style } from '@vanilla-extract/css';
 
 export const body = style({
   display: 'flex',
   flexDirection: 'column',
-  height: '100vh',
+  height: '100svh',
 });
 
 export const slideImageSection = style({
+  height: 'calc(100% - 17rem)',
   display: 'flex',
+  flexDirection: 'column',
   justifyContent: 'center',
   alignItems: 'center',
   width: '100%',
-  marginTop: '25%', //
-});
-
-export const slideContentSection = style({
-  width: '100%',
-  display: 'flex',
-  flex: 1,
-  padding: '3.2rem 3.6rem 3.2rem 3.6rem', //
-  flexDirection: 'column',
-  zIndex: themeVars.zIndex.content,
-});
-
-export const Container = style({
-  display: 'flex',
-  width: '100%',
-  height: '100%',
-  flexDirection: 'column',
-  alignItems: 'flex-start',
-  justifyContent: 'space-between',
+  marginTop: '6svh',
 });
 
 export const contentTextContainer = style({
+  width: '100%',
+  padding: '3.2rem 3.6rem 3.2rem 3.6rem',
   display: 'flex',
   flexDirection: 'column',
-  gap: '1.5rem', //
+  alignItems: 'flex-start',
+  justifyContent: 'space-between',
+  gap: '1.5rem',
   alignSelf: 'stretch',
 });
 
 export const contentHeader = style({
-  display: 'flex',
   width: '100%',
+  display: 'flex',
   flexDirection: 'column',
   alignItems: 'flex-start',
-  gap: '1.6rem', //
+  gap: '1.6rem',
 });
 
 export const bottomContainer = style({
+  width: '100%',
+  maxWidth: '43rem',
+  padding: '2.4rem 1.6rem 2.4rem 1.6rem',
+  position: 'fixed',
+  bottom: 0,
   display: 'flex',
   flexDirection: 'column',
   alignItems: 'center',
+  justifyContent: 'center',
   gap: '3.2rem',
   alignSelf: 'stretch',
 });
