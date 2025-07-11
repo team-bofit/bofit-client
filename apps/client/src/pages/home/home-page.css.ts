1117c48c28820872ba267bee82065381f9f07c84
minjeoong <orange_mj@naver.com>
2025-07-12 04:17:52 +0900

    feat: user icon color 가능하도록 수정

--- apps/client/src/pages/home/home-page.css.ts
+++ apps/client/src/pages/home/home-page.css.ts
@@ -2,13 +2,14 @@ import { themeVars } from '@bds/ui/styles';
 import { style } from '@vanilla-extract/css';
 
 export const homePage = style({
+  zIndex: themeVars.zIndex.base,
   backgroundColor: themeVars.color.primary500,
   height: '100vh',
 });
 
 export const userIcon = style({
-  width: '2.4rem',
-  height: '2.4rem',
+  width: '2rem',
+  height: '2rem',
   color: themeVars.color.white,
   background: themeVars.color.primary500,
 });
