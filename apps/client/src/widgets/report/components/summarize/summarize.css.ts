3c07a404fb331f9a5094d984a9462f75ef7ee8e1
1jiwoo27 <chloey1227@naver.com>
2025-07-12 04:50:16 +0900

    feat: 보험 추천 리포트 Summarize 컴포넌트 구현

---
+++ apps/client/src/widgets/report/components/summarize/summarize.css.ts
@@ -0,0 +1,69 @@
+import { themeVars } from '@bds/ui/styles';
+import { style } from '@vanilla-extract/css';
+
+export const summarizeContainer = style({
+  width: '100%',
+  display: 'flex',
+  position: 'relative',
+});
+
+export const backgroundImage = style({
+  zIndex: themeVars.zIndex.base,
+  position: 'absolute',
+  top: '1.4rem',
+  right: '0',
+  display: 'flex',
+  width: '17rem',
+  height: '17rem',
+  justifyContent: 'center',
+  alignItems: 'center',
+});
+
+export const topContainer = style({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-end',
+  gap: '0.8rem',
+  alignSelf: 'stretch',
+});
+
+export const insuranceContainer = style({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex',
+  gap: '0.6rem',
+  alignSelf: 'stretch',
+});
+
+export const titleContainer = style({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+  gap: '0.4rem',
+  alignSelf: 'stretch',
+});
+
+export const chipContainer = style({
+  display: 'flex',
+  flexDirection: 'row',
+  gap: '0.4rem',
+});
+
+export const infoContainer = style({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-end',
+  gap: '0.4rem',
+  alignSelf: 'stretch',
+});
+
+export const contentContainer = style({
+  position: 'relative',
+  zIndex: themeVars.zIndex.content,
+  margin: '3.2rem auto 1.6rem auto',
+  display: 'flex',
+  width: '34.3rem',
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+  gap: '1.6rem',
+});
---
+++ apps/client/src/widgets/report/components/summarize/summarize.tsx
@@ -0,0 +1,70 @@
+import { Chip } from '@bds/ui';
+
+import InsuranceSubtitle from '@shared/components/insurance-subtitle/insurance-subtitle';
+import InsuranceTitle from '@shared/components/insurance-title/insurance-title';
+
+import Maturity from '../maturity/maturity';
+import Price from '../price/price';
+import Recommend from '../recommend/recommend';
+
+import * as styles from './summarize.css';
+
+const reasonList = [
+  '운전이 잦아 상해 위험 대비가 필요해요',
+  '근골격계 이력으로 병원비 보장을 강화했어요',
+  '월 7만 원 이하로 꼭 필요한 보장만 담았어요',
+  '미혼인 점을 참고해 실제 치료비 중심으로 구성했어요',
+];
+
+const Summarize = () => {
+  return (
+    <div className={styles.summarizeContainer}>
+      <img
+        className={styles.backgroundImage}
+        src="./3d_shield.webp"
+        alt="Sheild"
+      />
+      <div className={styles.contentContainer}>
+        <div className={styles.topContainer}>
+          <div className={styles.insuranceContainer}>
+            <div className={styles.titleContainer}>
+              <InsuranceSubtitle
+                name="OO"
+                type="report"
+                fontColor="primary500"
+                fontStyle="m_16"
+              />
+              <InsuranceTitle
+                fontColor="gray900"
+                fontStyle="eb_24"
+                company="OO보험사"
+                name="OO보험"
+              />
+            </div>
+            <div className={styles.chipContainer}>
+              <Chip
+                label="# 중대 질환 든든 보장"
+                fontColor="gray"
+                backgroundColor="gray"
+                shape="rounded"
+              />
+              <Chip
+                label="# 합리적인 보험료"
+                fontColor="gray"
+                backgroundColor="gray"
+                shape="rounded"
+              />
+            </div>
+          </div>
+          <div className={styles.infoContainer}>
+            <Maturity age={100} />
+            <Price price={53479} />
+          </div>
+        </div>
+        <Recommend reasonList={reasonList} />
+      </div>
+    </div>
+  );
+};
+
+export default Summarize;
