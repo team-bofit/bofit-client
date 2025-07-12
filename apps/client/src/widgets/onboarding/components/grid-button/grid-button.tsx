69224435682380400c76e10dbce317fae59cf0c5
hansoojeongsj <tnwjd3758@gmail.com>
2025-07-12 17:10:23 +0900

    chore: 파일 세팅

---
+++ apps/client/src/widgets/onboarding/components/grid-button/grid-button.tsx
@@ -0,0 +1,13 @@
+import { Title } from '@bds/ui';
+
+const GridButton = () => {
+  return (
+    <>
+      <Title fontStyle={'bd_sm'}>
+        {`최근 5년 이내 병원에서 다음 질병을\n 진단 또는 치료받은 적 있나요?`}
+      </Title>
+    </>
+  );
+};
+
+export default GridButton;
