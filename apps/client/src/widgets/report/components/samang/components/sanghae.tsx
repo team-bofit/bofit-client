e4a44a7f3c7d6c06ca6a2ca19dc25e1c0519755e
jogpfls <joerin4177@gmail.com>
2025-07-13 05:04:31 +0900

    feat: 상해사망 컴포넌트 구현

---
+++ apps/client/src/widgets/report/components/samang/components/sanghae.tsx
@@ -0,0 +1,42 @@
+import { Alert } from '@bds/ui';
+
+import { ALERT } from '@widgets/report/constant/alert-content';
+
+import { Accordion } from '../../accordion/accordion';
+import Graph from '../../graph/graph';
+
+const sanghaeData = {
+  displayName: '상해사망',
+  surgery: {
+    productCoverage: 0,
+    averageCoverage: 50,
+  },
+};
+
+const Sanghae = () => {
+  const isZero = sanghaeData.surgery.productCoverage == 0;
+  return (
+    <Accordion>
+      <Accordion.Header type="강력">{sanghaeData.displayName}</Accordion.Header>
+      <Accordion.Panel>
+        {isZero ? (
+          <Alert
+            type="additional"
+            iconName="info_warning"
+            iconSize="2rem"
+            alertHeader={ALERT.HEADER}
+            alertContents={ALERT.CONTENTS}
+            highlight={sanghaeData.displayName}
+          />
+        ) : (
+          <Graph
+            average={sanghaeData.surgery.averageCoverage}
+            current={sanghaeData.surgery.productCoverage}
+          />
+        )}
+      </Accordion.Panel>
+    </Accordion>
+  );
+};
+
+export default Sanghae;
