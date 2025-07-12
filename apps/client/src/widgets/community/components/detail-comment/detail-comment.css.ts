e0f3ab9940173ce1855cb534ac75913f5d8173bd
jeonghoon11 <jih361@naver.com>
2025-07-13 03:48:58 +0900

    feat: 커뮤니티 페이지 목데이터 추가

---
+++ apps/client/src/widgets/community/components/detail-comment/detail-comment.css.ts
@@ -0,0 +1,58 @@
+import { themeVars } from '@bds/ui/styles';
+import { style } from '@vanilla-extract/css';
+
+export const container = style({
+  display: 'flex',
+  flexDirection: 'column',
+  paddingBottom: '0.8rem',
+  borderBottom: `1px solid ${themeVars.color.gray100}`,
+  gap: '2rem',
+});
+
+export const contentBox = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '1.2rem',
+});
+
+export const userInfo = style({
+  display: 'flex',
+  justifyContent: 'space-between',
+});
+
+export const userInfoLeft = style({
+  display: 'flex',
+  gap: '0.6rem',
+});
+
+export const infoContent = style({
+  display: 'flex',
+  alignItems: 'center',
+  gap: '0.4rem',
+});
+
+export const nickName = style({
+  color: themeVars.color.gray800,
+  ...themeVars.fontStyles.body1_m_12,
+});
+
+export const point = style({
+  color: themeVars.color.gray800,
+  ...themeVars.fontStyles.body1_m_12,
+});
+
+export const createdAt = style({
+  color: themeVars.color.gray800,
+  ...themeVars.fontStyles.body1_m_12,
+});
+
+export const commentNum = style({
+  display: 'flex',
+  gap: '0.4rem',
+  alignItems: 'center',
+});
+
+export const commentNumColor = style({
+  ...themeVars.fontStyles.body1_m_12,
+  color: themeVars.color.gray600,
+});
---
+++ apps/client/src/widgets/community/components/detail-comment/detail-comment.tsx
@@ -0,0 +1,50 @@
+import { Avatar, Content, Title } from '@bds/ui';
+import { Icon } from '@bds/ui/icons';
+
+import { POINT } from '@widgets/community/constant/detail-content';
+import { getTimeAgo } from '@widgets/community/utils/getTimeAgo';
+
+import * as styles from './detail-comment.css';
+
+interface DetailCommentProps {
+  title: string;
+  text: string;
+  writerNickName: string;
+  createdAt: string;
+  commentNum: number;
+  onClick: () => void;
+}
+
+const DetailComment = ({
+  title,
+  text,
+  writerNickName,
+  createdAt,
+  commentNum,
+  onClick,
+}: DetailCommentProps) => {
+  return (
+    <div className={styles.container} onClick={onClick}>
+      <div className={styles.contentBox}>
+        <Title fontStyle="bd_sm">{title}</Title>
+        <Content text={text} length="md" />
+      </div>
+      <div className={styles.userInfo}>
+        <div className={styles.userInfoLeft}>
+          <Avatar size="sm" />
+          <div className={styles.infoContent}>
+            <p className={styles.nickName}>{writerNickName}</p>
+            <p className={styles.point}>{POINT}</p>
+            <p className={styles.createdAt}>{getTimeAgo(createdAt)}</p>
+          </div>
+        </div>
+        <div className={styles.commentNum}>
+          <Icon name="chat_square" width="2rem" height="2rem" color="gray600" />
+          <div className={styles.commentNumColor}>{commentNum}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DetailComment;
