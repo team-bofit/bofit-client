b707bfd31708a07734f4926fa035712a76aa9540
1jiwoo27 <chloey1227@naver.com>
2025-07-12 21:07:17 +0900

    feat: price 변환 함수 추가

--- apps/client/src/widgets/report/components/price/price.tsx
+++ apps/client/src/widgets/report/components/price/price.tsx
@@ -1,17 +1,23 @@
 import * as styles from './price.css';
 
 interface PriceProps {
-  price: number; // TODO 명세 필드명 반영
+  price: number; // @todo 명세 필드명 반영
 }
+
+const formatPrice = (price: number): string => {
+  return price.toLocaleString('ko-KR');
+};
 
 const MONTH = '월';
 const WON = '원';
 
 const Price = ({ price }: PriceProps) => {
+  const formattedPrice = formatPrice(price);
+
   return (
     <div className={styles.priceContainer}>
       <p className={styles.month}>{MONTH}</p>
-      <p className={styles.price}>{price}</p>
+      <p className={styles.price}>{formattedPrice}</p>
       <p>{WON}</p>
     </div>
   );
