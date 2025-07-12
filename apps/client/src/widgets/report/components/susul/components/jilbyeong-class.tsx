5b8e4d97dd3b26c17c860fe49fee40408f9c0f03
jogpfls <joerin4177@gmail.com>
2025-07-13 01:34:08 +0900

    feat: 질병 종 수술비 컴포넌트 구현

---
+++ apps/client/src/widgets/report/components/susul/components/jilbyeong-class.tsx
@@ -0,0 +1,51 @@
+import { Accordion } from '../../accordion/accordion';
+import Class from '../../class/class';
+
+const jilbyiongClassData = {
+  displayName: '질병 종 수술비',
+  surgeryType: {
+    type1: {
+      productCoverage: 10,
+      averageCoverage: 20,
+    },
+    type2: {
+      productCoverage: 25,
+      averageCoverage: 30,
+    },
+    type3: {
+      productCoverage: 120,
+      averageCoverage: 100,
+    },
+    type4: {
+      productCoverage: 250,
+      averageCoverage: 500,
+    },
+    type5: {
+      productCoverage: 1200,
+      averageCoverage: 1000,
+    },
+  },
+};
+
+const JilbyeongClass = () => {
+  const surgeryList = Object.values(jilbyiongClassData.surgeryType);
+
+  const averageValues = surgeryList.map((surgery) => surgery.averageCoverage);
+  const guaranteeValues = surgeryList.map((surgery) => surgery.productCoverage);
+
+  return (
+    <div>
+      <Accordion>
+        <Accordion.Header type="강력">질병 종 수술비</Accordion.Header>
+        <Accordion.Panel>
+          <Class
+            averageValues={averageValues}
+            guaranteeValues={guaranteeValues}
+          />
+        </Accordion.Panel>
+      </Accordion>
+    </div>
+  );
+};
+
+export default JilbyeongClass;
