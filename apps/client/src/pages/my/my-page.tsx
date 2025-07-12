07474fdb3f7049a48d0167b3ce3f1062c2c3cdd8
gwagjiug <kwjo0228@naver.com>
2025-07-13 04:31:40 +0900

    feat: my 페이지 퍼블리싱

--- apps/client/src/pages/my/my-page.tsx
+++ apps/client/src/pages/my/my-page.tsx
@@ -1,5 +1,20 @@
+import { Navigation } from '@bds/ui';
+import { Icon } from '@bds/ui/icons';
+
+import User from '@widgets/mypage/user';
+
 const MyPage = () => {
-  return <div>MyPage</div>;
+  return (
+    <>
+      <Navigation
+        title={'지욱님 반가워요'}
+        rightIcon={<Icon name="home" color="white" />}
+        backgroundColor="primary"
+        textColor="white"
+      />
+      <User />
+    </>
+  );
 };
 
 export default MyPage;
