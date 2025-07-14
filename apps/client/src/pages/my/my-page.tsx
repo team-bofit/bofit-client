e4269a44848a314cd88dd1322ed4fad091d8abac
gwagjiug <kwjo0228@naver.com>
2025-07-14 19:26:26 +0900

    feat: navigate 함수 추가

--- apps/client/src/pages/my/my-page.tsx
+++ apps/client/src/pages/my/my-page.tsx
@@ -1,19 +1,35 @@
+import { useNavigate } from 'react-router';
+
 import { Navigation } from '@bds/ui';
 import { Icon } from '@bds/ui/icons';
 
 import Body from '@widgets/mypage/body';
 
+import { routePath } from '@shared/router/path';
+
 const MyPage = () => {
+  const navigate = useNavigate();
+
+  const handleNavigate = (route: string) => {
+    navigate(route);
+  };
+
   return (
     <>
       <Navigation
         title={'지욱님 반가워요!'}
-        rightIcon={<Icon name="home" color="white" />}
+        rightIcon={
+          <Icon
+            name="home"
+            color="white"
+            onClick={() => handleNavigate(routePath.HOME)}
+          />
+        }
         backgroundColor="primary"
         textColor="white"
         hasZIndex={true}
       />
-      <Body nickname={'지욱'} />
+      <Body nickname="지욱" onClick={() => handleNavigate(routePath.REPORT)} />
     </>
   );
 };
--- apps/client/src/widgets/mypage/body.tsx
+++ apps/client/src/widgets/mypage/body.tsx
@@ -1,21 +1,29 @@
 import { Avatar, Button } from '@bds/ui';
 
+import { routePath } from '@shared/router/path';
+
 import Preview from './preview';
 
 import * as styles from './body.css';
 
 interface ContentProps {
   nickname: string;
+  onClick: (route: string) => void;
 }
 
-const Body = ({ nickname }: ContentProps) => {
+const Body = ({ nickname, onClick }: ContentProps) => {
   return (
     <section className={styles.userSection}>
       <div className={styles.userContent}>
         <Avatar size={'lg'} />
         <div className={styles.contentName}>
           {nickname}
-          <Button variant="white_fill">내 보험 추천 리포트</Button>
+          <Button
+            variant="white_fill"
+            onClick={() => onClick(routePath.REPORT)}
+          >
+            내 보험 추천 리포트
+          </Button>
         </div>
       </div>
       <Preview />
