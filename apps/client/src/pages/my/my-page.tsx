8a16dc9f5ee94d86676d0d3b468ec2ec31d07146
gwagjiug <kwjo0228@naver.com>
2025-07-14 18:49:57 +0900

    faet: body 추가

--- apps/client/src/pages/my/my-page.tsx
+++ apps/client/src/pages/my/my-page.tsx
@@ -1,18 +1,19 @@
 import { Navigation } from '@bds/ui';
 import { Icon } from '@bds/ui/icons';
 
-import User from '@widgets/mypage/user';
+import Bpdy from '@widgets/mypage/body';
 
 const MyPage = () => {
   return (
     <>
       <Navigation
-        title={'지욱님 반가워요'}
+        title={'지욱님 반가워요!'}
         rightIcon={<Icon name="home" color="white" />}
         backgroundColor="primary"
         textColor="white"
+        hasZIndex={true}
       />
-      <User />
+      <Bpdy nickname={'지욱'} />
     </>
   );
 };
