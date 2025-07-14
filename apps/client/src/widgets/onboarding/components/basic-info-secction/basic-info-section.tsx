d07f19b694816988f9a5259b67a861162e5ceecf
1jiwoo27 <101466143+1jiwoo27@users.noreply.github.com>
2025-07-14 18:29:16 +0900

    Feat(client): Basic Info Section 컴포넌트 구현 (#170)
    * feat: input 컴포넌트 배경색 추가
    
    * feat: button 컴포넌트 variant 추가
    
    * feat: basic info section 컴포넌트 구현
    
    * chore: eslint 오류 수정
    
    * chore: 상수 사용
    
    * refactor: State type에서 interface로 변경
    
    * refactor: 공통 핸들러 함수로 dispatch 로직 정리

---
+++ apps/client/src/widgets/onboarding/components/basic-info-secction/basic-info-section.tsx
@@ -0,0 +1,244 @@
+import { useReducer } from 'react';
+
+import { Button, Input } from '@bds/ui';
+
+import DropDown from '../dropdown/dropdown';
+
+import * as styles from './basic-info-section.css';
+
+const LABEL = {
+  NAME: '이름',
+  BIRTHDATE: '생년월일',
+  GENDER: '성별',
+  OCCUPATION: '어떤 직업에 종사하고 계신가요?',
+  MARRIED: '기혼자이신가요?',
+  CHILD: '자녀가 있으신가요?',
+  DRIVER: '운전하시나요?',
+};
+
+const OPTION = {
+  YES: '예',
+  NO: '아니오',
+  MALE: '남성',
+  FEMALE: '여성',
+  NAME_PLACEHOLDER: '이름을 작성해주세요.',
+  YEAR: '년',
+  MONTH: '월',
+  DAY: '일',
+};
+
+interface State {
+  name: string;
+  birthYear: string;
+  birthMonth: string;
+  birthDay: string;
+  gender: '남성' | '여성' | null;
+  isMarried: boolean | null;
+  hasChild: boolean | null;
+  isDriver: boolean | null;
+}
+
+type Action =
+  | { type: 'SET_NAME'; payload: string }
+  | { type: 'SET_BIRTH_YEAR'; payload: string }
+  | { type: 'SET_BIRTH_MONTH'; payload: string }
+  | { type: 'SET_BIRTH_DAY'; payload: string }
+  | { type: 'SET_GENDER'; payload: '남성' | '여성' }
+  | { type: 'SET_IS_MARRIED'; payload: boolean }
+  | { type: 'SET_HAS_CHILD'; payload: boolean }
+  | { type: 'SET_IS_DRIVER'; payload: boolean };
+
+const initialState: State = {
+  name: '',
+  birthYear: '',
+  birthMonth: '',
+  birthDay: '',
+  gender: '여성',
+  isMarried: false,
+  hasChild: false,
+  isDriver: false,
+};
+
+const reducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case 'SET_NAME':
+      return { ...state, name: action.payload };
+    case 'SET_BIRTH_YEAR':
+      return { ...state, birthYear: action.payload };
+    case 'SET_BIRTH_MONTH':
+      return { ...state, birthMonth: action.payload };
+    case 'SET_BIRTH_DAY':
+      return { ...state, birthDay: action.payload };
+    case 'SET_GENDER':
+      return { ...state, gender: action.payload };
+    case 'SET_IS_MARRIED':
+      return { ...state, isMarried: action.payload };
+    case 'SET_HAS_CHILD':
+      return { ...state, hasChild: action.payload };
+    case 'SET_IS_DRIVER':
+      return { ...state, isDriver: action.payload };
+    default:
+      return state;
+  }
+};
+
+const BasicInfoSection = () => {
+  const handleChange =
+    (
+      type: 'SET_NAME' | 'SET_BIRTH_YEAR' | 'SET_BIRTH_MONTH' | 'SET_BIRTH_DAY',
+    ) =>
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      dispatch({ type, payload: e.target.value } as Action);
+    };
+
+  const handleClick =
+    <T extends Action['payload']>(type: Action['type'], payload: T) =>
+    () => {
+      dispatch({ type, payload } as Action);
+    };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <section className={styles.basicContainer}>
+      <div className={styles.fieldContainer}>
+        <p className={styles.fieldNameLabel}>{LABEL.NAME}</p>
+        <Input
+          value={state.name}
+          onChange={handleChange('SET_NAME')}
+          bgColor="background"
+          placeholder={OPTION.NAME_PLACEHOLDER}
+        />
+      </div>
+
+      <div className={styles.fieldContainer}>
+        <p className={styles.fieldNameLabel}>{LABEL.BIRTHDATE}</p>
+        <div className={styles.birthdateContainer}>
+          <div className={styles.birthInputContainer}>
+            <div className={styles.birthdateInput}>
+              <Input
+                value={state.birthYear}
+                onChange={handleChange('SET_BIRTH_YEAR')}
+                placeholder="YYYY"
+                maxLength={4}
+                bgColor="background"
+              />
+            </div>
+            <span className={styles.birthdateLabel}>{OPTION.YEAR}</span>
+          </div>
+          <div className={styles.birthInputContainer}>
+            <div className={styles.birthdateInput}>
+              <Input
+                value={state.birthMonth}
+                onChange={handleChange('SET_BIRTH_MONTH')}
+                placeholder="MM"
+                maxLength={2}
+                bgColor="background"
+              />
+            </div>
+            <span className={styles.birthdateLabel}>{OPTION.MONTH}</span>
+          </div>
+          <div className={styles.birthInputContainer}>
+            <div className={styles.birthdateInput}>
+              <Input
+                value={state.birthDay}
+                onChange={handleChange('SET_BIRTH_DAY')}
+                placeholder="DD"
+                maxLength={2}
+                bgColor="background"
+              />
+            </div>
+            <span className={styles.birthdateLabel}>{OPTION.DAY}</span>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.fieldContainer}>
+        <p className={styles.fieldNameLabel}>{LABEL.GENDER}</p>
+        <div className={styles.buttonContainer}>
+          <Button
+            size="lg"
+            variant={state.gender === OPTION.MALE ? 'selected' : 'unselected'}
+            onClick={handleClick('SET_GENDER', '남성')}
+          >
+            {OPTION.MALE}
+          </Button>
+          <Button
+            size="lg"
+            variant={state.gender === OPTION.FEMALE ? 'selected' : 'unselected'}
+            onClick={handleClick('SET_GENDER', '여성')}
+          >
+            {OPTION.FEMALE}
+          </Button>
+        </div>
+      </div>
+
+      <div className={styles.fieldContainer}>
+        <p className={styles.fieldNameLabel}>{LABEL.OCCUPATION}</p>
+        <DropDown />
+      </div>
+
+      <div className={styles.fieldContainer}>
+        <p className={styles.fieldNameLabel}>{LABEL.MARRIED}</p>
+        <div className={styles.buttonContainer}>
+          <Button
+            size="lg"
+            variant={state.isMarried === true ? 'selected' : 'unselected'}
+            onClick={handleClick('SET_IS_MARRIED', true)}
+          >
+            {OPTION.YES}
+          </Button>
+          <Button
+            size="lg"
+            variant={state.isMarried === false ? 'selected' : 'unselected'}
+            onClick={handleClick('SET_IS_MARRIED', false)}
+          >
+            {OPTION.NO}
+          </Button>
+        </div>
+      </div>
+
+      <div className={styles.fieldContainer}>
+        <p className={styles.fieldNameLabel}>{LABEL.CHILD}</p>
+        <div className={styles.buttonContainer}>
+          <Button
+            size="lg"
+            variant={state.hasChild === true ? 'selected' : 'unselected'}
+            onClick={handleClick('SET_HAS_CHILD', true)}
+          >
+            {OPTION.YES}
+          </Button>
+          <Button
+            size="lg"
+            variant={state.hasChild === false ? 'selected' : 'unselected'}
+            onClick={handleClick('SET_HAS_CHILD', false)}
+          >
+            {OPTION.NO}
+          </Button>
+        </div>
+      </div>
+
+      <div className={styles.fieldContainer}>
+        <p className={styles.fieldNameLabel}>{LABEL.DRIVER}</p>
+        <div className={styles.buttonContainer}>
+          <Button
+            size="lg"
+            variant={state.isDriver === true ? 'selected' : 'unselected'}
+            onClick={handleClick('SET_IS_DRIVER', true)}
+          >
+            {OPTION.YES}
+          </Button>
+          <Button
+            size="lg"
+            variant={state.isDriver === false ? 'selected' : 'unselected'}
+            onClick={handleClick('SET_IS_DRIVER', false)}
+          >
+            {OPTION.NO}
+          </Button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default BasicInfoSection;
