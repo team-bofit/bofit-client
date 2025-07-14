f324fb63d005aa5b61b5b4b883f82901b24cc33e
jogpfls <joerin4177@gmail.com>
2025-07-15 00:03:28 +0900

    feat: 보험 추천 리포트 페이지 병합

--- apps/client/src/pages/report/report-page.tsx
+++ apps/client/src/pages/report/report-page.tsx
@@ -1,5 +1,14 @@
+import ReportDetail from '@widgets/report/components/report-detail/report-detail';
+import Summarize from '@widgets/report/components/summarize/summarize';
+
+import * as styles from './report-page.css';
 const ReportPage = () => {
-  return <div>ReportPage</div>;
+  return (
+    <div className={styles.container}>
+      <Summarize />
+      <ReportDetail />
+    </div>
+  );
 };
 
 export default ReportPage;
