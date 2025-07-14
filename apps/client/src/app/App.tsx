64b1d7d5de08c0213587a07f9620d7fb696a5688
gwagjiug <kwjo0228@naver.com>
2025-07-15 04:14:23 +0900

    fix: error 페이지 rootStyle 적용

--- apps/client/src/app/App.tsx
+++ apps/client/src/app/App.tsx
@@ -2,7 +2,7 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider } from 'react-router';
 
-import { ModalContainer, ToastContainer } from '@bds/ui';
+import { ModalContainer, ThemeProvider, ToastContainer } from '@bds/ui';
 
 import { GlobalErrorBoundary } from '@shared/router/global-error-boundary.tsx';
 import { router } from '@shared/router/router';
@@ -10,14 +10,16 @@ import { queryClient } from '@shared/utils/query-client';
 
 function App() {
   return (
-    <GlobalErrorBoundary>
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-        <ToastContainer />
-        <ModalContainer />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-    </GlobalErrorBoundary>
+    <ThemeProvider>
+      <GlobalErrorBoundary>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+          <ToastContainer />
+          <ModalContainer />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </GlobalErrorBoundary>
+    </ThemeProvider>
   );
 }
 
--- apps/client/src/shared/router/global-layout.tsx
+++ apps/client/src/shared/router/global-layout.tsx
@@ -1,21 +1,21 @@
 import { Suspense } from 'react';
 import { matchPath, Outlet, useLocation } from 'react-router-dom';
 
-import { ThemeProvider } from '@bds/ui';
 import { noRootShadow, rootStyle } from '@bds/ui/styles';
 
 import { routePath } from './path';
 
 export default function GlobalLayout() {
   const location = useLocation();
-  const isLayoutFree = matchPath(routePath.MY, location.pathname);
+  const isLayoutFree = !!matchPath(
+    { path: routePath.MY, end: true },
+    location.pathname,
+  );
   return (
-    <ThemeProvider
-      className={`${rootStyle} ${isLayoutFree ? noRootShadow : ''}`}
-    >
+    <div className={`${isLayoutFree ? noRootShadow : rootStyle}`}>
       <Suspense>
         <Outlet />
       </Suspense>
-    </ThemeProvider>
+    </div>
   );
 }
