9c76932bcc197f20a98c77c3fa89c5e2e857dc21
jogpfls <joerin4177@gmail.com>
2025-07-16 06:01:56 +0900

    feat: 큰병 컴포넌트 api 연동

--- apps/client/src/widgets/report/components/keunbyeong/keunbyeong.tsx
+++ apps/client/src/widgets/report/components/keunbyeong/keunbyeong.tsx
@@ -1,3 +1,8 @@
+import { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+
+import { INSURANCE_QUERY_OPTION } from '@shared/api/domain/report/queries';
+
 import Divider from '../divider/divider';
 import Info from '../info/info';
 import Cancer from './components/cancer';
@@ -6,25 +11,40 @@ import Shimjang from './components/shimjang';
 
 import * as styles from './keunbyeong.css';
 
-const keunbyeongData = {
+const keunbyeongMockData = {
   additional_info:
     '3대 중증질환(암, 뇌혈관, 심장)은 발병률이 높고, 치료비 부담도 커요. 진단 즉시 목돈(진단비)이 지급돼 경제적 부담을 줄여줘요.',
 };
 const TEXT_TITLE = '큰병';
+const TEST_REPORT_ID = '2281ccfc-1f10-4798-b3ad-6468b357b789';
 
 const Keunbyeong = () => {
+  const [accordionCategory, setAccordionCategory] = useState('');
+
+  const handleSelectClick = (category: string) => {
+    setAccordionCategory(category);
+  };
+
+  const { data: keunbyeongData } = useQuery({
+    ...INSURANCE_QUERY_OPTION.REPORT_KEUNBYEONG(
+      TEST_REPORT_ID,
+      accordionCategory,
+    ),
+    enabled: !!accordionCategory,
+  });
+
   return (
     <div className={styles.dividerContainer}>
       <Divider>{TEXT_TITLE}</Divider>
       <div className={styles.contentsContainer}>
         <Info
-          description={keunbyeongData.additional_info}
+          description={keunbyeongMockData.additional_info}
           size="md"
           iconSize="2rem"
         />
-        <Cancer />
-        <Noehyeolgwan />
-        <Shimjang />
+        <Cancer onClick={handleSelectClick} data={keunbyeongData?.data} />
+        <Noehyeolgwan onClick={handleSelectClick} data={keunbyeongData?.data} />
+        <Shimjang onClick={handleSelectClick} data={keunbyeongData?.data} />
       </div>
     </div>
   );
