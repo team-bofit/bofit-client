ed83309a086a1469569d02e3fffa09104ea9cc3c
minjeoong <orange_mj@naver.com>
2025-07-15 16:46:42 +0900

    fix: mypage 임시코드 롤백

--- apps/client/src/pages/my/my-page.tsx
+++ apps/client/src/pages/my/my-page.tsx
@@ -1,26 +1,5 @@
-import { useState } from 'react';
-
-import { Slider } from '@bds/ui';
-
 const MyPage = () => {
-  const [value, setValue] = useState<[number, number]>([7, 15]);
-
-  const handleChange = (newValue: [number, number]) => {
-    setValue(newValue);
-  };
-  return (
-    <div>
-      <div style={{ padding: '2rem' }}>
-        <Slider
-          min={0}
-          max={30}
-          value={value}
-          onChange={handleChange}
-          defaultValue={[7, 15]}
-        />
-      </div>
-    </div>
-  );
+  return <div>MyPage</div>;
 };
 
 export default MyPage;
