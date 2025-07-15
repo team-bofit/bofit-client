cfaca9a8017dd55eaec2663dc752a0462fd9961b
jeonghoon11 <jih361@naver.com>
2025-07-16 05:20:26 +0900

    fix: 주석 제거

--- apps/client/src/shared/api/domain/community/queries.ts
+++ apps/client/src/shared/api/domain/community/queries.ts
@@ -1,18 +1,6 @@
-// import { queryOptions } from '@tanstack/react-query';
-
 import { api } from '@shared/api/config/instance';
 import { END_POINT } from '@shared/constants/end-point';
-// import { POSTS_QUERY_KEY } from '@shared/constants/query-key';
 import { paths } from '@shared/types/schema';
-
-// export const POSTS_QUERY_OPTIONS = {
-//   POSTS: () => {
-//     return queryOptions({
-//       queryKey: POSTS_QUERY_KEY.POSTS(),
-//       queryFn: getPosts,
-//     });
-//   },
-// };
 
 export type communityListResponse =
   paths['/posts']['get']['responses']['200']['content']['*/*']['data'];
