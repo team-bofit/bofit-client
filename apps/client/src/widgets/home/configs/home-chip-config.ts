1d33a7d25f9b32b51b39d5039e2327a03fc1f6e4
minjeoong <orange_mj@naver.com>
2025-07-16 03:34:19 +0900

    feat: home-chip-config 파일 분리

---
+++ apps/client/src/widgets/home/configs/home-chip-config.ts
@@ -0,0 +1,58 @@
+export const homeChipConfig = [
+  {
+    icon: '3d_brain',
+    target: '뇌혈관질환',
+  },
+  {
+    icon: '3d_cancer',
+    target: '암',
+  },
+  {
+    icon: '3d_die',
+    target: '사망',
+  },
+  {
+    icon: '3d_disability',
+    target: '장해',
+  },
+  {
+    icon: '3d_heart',
+    target: '심장질환',
+  },
+  {
+    icon: '3d_hospital',
+    target: '입원',
+  },
+  {
+    icon: '3d_surgery',
+    target: '수술',
+  },
+  {
+    icon: '3d_brain',
+    target: '뇌혈관질환',
+  },
+  {
+    icon: '3d_cancer',
+    target: '암',
+  },
+  {
+    icon: '3d_die',
+    target: '사망',
+  },
+  {
+    icon: '3d_disability',
+    target: '장해',
+  },
+  {
+    icon: '3d_heart',
+    target: '심장질환',
+  },
+  {
+    icon: '3d_hospital',
+    target: '입원',
+  },
+  {
+    icon: '3d_surgery',
+    target: '수술비',
+  },
+];
