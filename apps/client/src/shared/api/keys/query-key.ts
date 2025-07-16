463d09374daddb3443ffd47c110cff7a397e7614
gwagjiug <kwjo0228@naver.com>
2025-07-17 06:09:31 +0900

    feat: 쿼리 키 추가 및 body 버튼 상수화

--- apps/client/src/shared/api/keys/query-key.ts
+++ apps/client/src/shared/api/keys/query-key.ts
@@ -7,10 +7,10 @@ export const USER_QUERY_KEY = {
   ALL: ['users'],
   PROFILE: () => [...USER_QUERY_KEY.ALL, 'profile'],
   JOBS: () => [...USER_QUERY_KEY.ALL, 'jobs'],
+  ME_POSTS: () => [...USER_QUERY_KEY.ALL, 'me-post'],
+  ME_COMMENTS: () => [...USER_QUERY_KEY.ALL, 'me-comments'],
 } as const;
 
 export const COMMUNITY_QUERY_KEY = {
   ALL: ['community'],
-  ME_POSTS: () => [...COMMUNITY_QUERY_KEY.ALL, 'me-post'],
-  ME_COMMENTS: () => [...COMMUNITY_QUERY_KEY.ALL, 'me-comments'],
 };
