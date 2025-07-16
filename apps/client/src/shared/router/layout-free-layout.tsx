346796ea54872e7dcc9444f5d85a1de927306d68
Minjeong Kim <88662427+minjeoong@users.noreply.github.com>
2025-07-17 01:12:24 +0900

    Fix(client): suspense 로딩이 전역적으로 적용이 되지 않는 문제 해결 (#221)

--- apps/client/src/shared/router/layout-free-layout.tsx
+++ apps/client/src/shared/router/layout-free-layout.tsx
@@ -1,4 +1,3 @@
-import { Suspense } from 'react';
 import { Outlet, ScrollRestoration } from 'react-router-dom';
 
 import { noRootShadow } from '@bds/ui/styles';
@@ -6,10 +5,8 @@ import { noRootShadow } from '@bds/ui/styles';
 export default function layoutFreeLayout() {
   return (
     <div className={noRootShadow}>
-      <Suspense>
-        <Outlet />
-        <ScrollRestoration />
-      </Suspense>
+      <Outlet />
+      <ScrollRestoration />
     </div>
   );
 }
