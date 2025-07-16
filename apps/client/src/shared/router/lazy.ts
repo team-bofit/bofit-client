ebf64ded2a22bcc02a36fb8f61b2c3cae0efec98
minjeoong <orange_mj@naver.com>
2025-07-16 12:07:40 +0900

    feat: lazy 로 route component 감싸기

--- apps/client/src/shared/router/lazy.ts
+++ apps/client/src/shared/router/lazy.ts
@@ -1,3 +1,28 @@
 import { lazy } from 'react';
 
 export const HomePage = lazy(() => import('@pages/home/home-page'));
+
+export const OnboardingPage = lazy(
+  () => import('@pages/onboarding/onboarding-page'),
+);
+
+export const CommunityPage = lazy(
+  () => import('@pages/community/community-page'),
+);
+export const CommunityWrite = lazy(
+  () => import('@pages/community/community-write/community-write'),
+);
+export const CommunityDetail = lazy(
+  () => import('@pages/community/community-detail/community-detail'),
+);
+
+export const MyPage = lazy(() => import('@pages/my/my-page'));
+
+export const ReportPage = lazy(() => import('@pages/report/report-page'));
+
+export const LoginPage = lazy(() => import('@pages/login/login-page'));
+export const LoginFallbackPage = lazy(
+  () => import('@pages/login-fallback/login-fallback-page'),
+);
+
+export const SplashPage = lazy(() => import('@pages/splash/splash-page'));
