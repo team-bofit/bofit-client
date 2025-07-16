baba6127e1718284680d560d2914293ce08096ba
gwagjiug <kwjo0228@naver.com>
2025-07-17 01:08:20 +0900

    feat: 엔드포인트 설정

--- apps/client/src/shared/api/config/end-point.ts
+++ apps/client/src/shared/api/config/end-point.ts
@@ -1,12 +1,12 @@
 export const END_POINT = {
   COMMUNITY: {
     POST_FEED: 'posts',
-    GET_ME_POSTS: 'users/me/posts',
-    GET_ME_COMMENTS: 'users/me/comments',
   },
   USER: {
     GET_USER_INFO: 'users/info',
     GET_USER_INFO_JOBS: 'user-infos/jobs',
+    GET_ME_POSTS: 'users/me/posts',
+    GET_ME_COMMENTS: 'users/me/comments',
   },
   INSURANCE: {
     GET_REPORT: (id: string) => `insurances/reports/${id}`,
