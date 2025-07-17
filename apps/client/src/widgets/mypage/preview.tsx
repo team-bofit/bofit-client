17e30374003dd2f8ec4a074d7f0857f1d8c04937
gwagjiug <kwjo0228@naver.com>
2025-07-17 23:06:55 +0900

    fix: 사용하지 않는 코드 제거

--- apps/client/src/widgets/mypage/preview.tsx
+++ apps/client/src/widgets/mypage/preview.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 import { useInfiniteQuery } from '@tanstack/react-query';
 import { useNavigate } from 'react-router';
 
