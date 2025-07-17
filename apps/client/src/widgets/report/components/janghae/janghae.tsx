b48c9b997020e2860b9de1d601a8fddd3ccb0e77
jogpfls <joerin4177@gmail.com>
2025-07-18 02:14:37 +0900

    feat: 장해 api 연동

--- apps/client/src/widgets/report/components/janghae/janghae.tsx
+++ apps/client/src/widgets/report/components/janghae/janghae.tsx
@@ -1,3 +1,10 @@
+import { useEffect, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+
+import { ACCORDION_CATEGORY } from '@widgets/report/constant/accordion-category-constant';
+
+import { INSURANCE_QUERY_OPTIONS } from '@shared/api/domain/report/queries';
+import { InsuranceJanghaeReport } from '@shared/api/types/types';
 import { components } from '@shared/types/schema';
 import { StatusType } from '@shared/types/type';
 
@@ -13,10 +20,40 @@ interface JanghaeProps {
 }
 
 const TEXT_TITLE = '장해';
+const TEST_REPORT_ID = '2281ccfc-1f10-4798-b3ad-6468b357b789';
 
-const COMPONENT = [{ Component: Jilbyeong }, { Component: Sanghae }] as const;
+const JANGHAE_COMPONENT = [
+  { Component: Jilbyeong, key: ACCORDION_CATEGORY.JANGHAE.JILBYEONG },
+  { Component: Sanghae, key: ACCORDION_CATEGORY.JANGHAE.SANGHAE },
+] as const;
 
 const Janghae = ({ sectionData }: JanghaeProps) => {
+  const [cachedDataMap, setCachedDataMap] = useState<
+    Partial<Record<string, InsuranceJanghaeReport['data']>>
+  >({});
+  const [accordionCategory, setAccordionCategory] = useState('');
+
+  const handleSelectClick = (category: string) => {
+    setAccordionCategory(category);
+  };
+
+  const { data: janghaeData } = useQuery({
+    ...INSURANCE_QUERY_OPTIONS.REPORT_JANGHAE(
+      TEST_REPORT_ID,
+      accordionCategory,
+    ),
+    enabled: !!accordionCategory && !cachedDataMap[accordionCategory],
+  });
+
+  useEffect(() => {
+    if (janghaeData?.data?.hyphenCase) {
+      setCachedDataMap((prev) => ({
+        ...prev,
+        [janghaeData?.data?.hyphenCase ?? '']: janghaeData.data,
+      }));
+    }
+  }, [janghaeData]);
+
   return (
     <div className={styles.container}>
       <Divider>{TEXT_TITLE}</Divider>
@@ -26,12 +63,15 @@ const Janghae = ({ sectionData }: JanghaeProps) => {
           size="md"
           iconSize="2rem"
         />
-        {sectionData?.statuses?.map(({ target, status }, index) => {
-          const Component = COMPONENT[index]?.Component;
-          return Component ? (
-            <Component target={target} status={status as StatusType} />
-          ) : null;
-        })}
+        {JANGHAE_COMPONENT.map(({ Component, key }, index) => (
+          <Component
+            key={key}
+            target={sectionData?.statuses?.[index].target}
+            status={sectionData?.statuses?.[index].status as StatusType}
+            onClick={handleSelectClick}
+            data={cachedDataMap[key]}
+          />
+        ))}
       </div>
     </div>
   );
