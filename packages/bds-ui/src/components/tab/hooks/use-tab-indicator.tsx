675599dad7da40fcb481e81d23e644ca28f07632
gwagjiug <kwjo0228@naver.com>
2025-07-17 16:56:17 +0900

    feat: tab value change props 추가

--- packages/bds-ui/src/components/tab/hooks/use-tab-indicator.tsx
+++ packages/bds-ui/src/components/tab/hooks/use-tab-indicator.tsx
@@ -1,6 +1,9 @@
 import { useLayoutEffect, useRef, useState } from 'react';
 
-export const useTabIndicator = (initialValue: string) => {
+export const useTabIndicator = (
+  initialValue: string,
+  onValueChange?: (value: string) => void,
+) => {
   const [selectedTab, setSelectedTab] = useState(initialValue);
   const tabRefs = useRef<Record<string, HTMLLIElement | null>>({});
   const [translateX, setTranslateX] = useState(0);
@@ -15,8 +18,17 @@ export const useTabIndicator = (initialValue: string) => {
     }
   }, [selectedTab]);
 
+  const setSelectedTabAndNotify = (value: string) => {
+    setSelectedTab(value);
+    onValueChange?.(value);
+  };
+
   return {
     translateX,
-    contextValue: { selectedTab, setSelectedTab, tabRefs },
+    contextValue: {
+      selectedTab,
+      setSelectedTab: setSelectedTabAndNotify,
+      tabRefs,
+    },
   };
 };
