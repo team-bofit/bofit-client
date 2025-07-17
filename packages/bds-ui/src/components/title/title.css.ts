7c90f06392f5d2a3c6f351069c3eaf3db6cb3b6c
gwagjiug <kwjo0228@naver.com>
2025-07-17 16:55:54 +0900

    feat: title css 수정

--- packages/bds-ui/src/components/title/title.css.ts
+++ packages/bds-ui/src/components/title/title.css.ts
@@ -2,7 +2,6 @@ import { styleVariants } from '@vanilla-extract/css';
 import { style } from '@vanilla-extract/css';
 
 import { themeVars } from '../../styles/theme.css';
-import { color } from '../../styles/tokens/color.css';
 
 export const base = style({
   color: themeVars.color.primary900,
@@ -14,4 +13,5 @@ export const titleVariants = styleVariants({
   bd_sm: [base, { ...themeVars.fontStyles.head2_b_16 }],
   bd_md: [base, { ...themeVars.fontStyles.head2_b_20 }],
   eb_md: [base, { ...themeVars.fontStyles.head_eb_20 }],
+  tt_md: [base, { ...themeVars.fontStyles.title_sb_16 }],
 });
