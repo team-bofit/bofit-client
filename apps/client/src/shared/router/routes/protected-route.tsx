03239dbaf6b31a6194d61efae4ef9fdb9fd61c41
hansoojeongsj <tnwjd3758@gmail.com>
2025-07-18 03:53:04 +0900

    fix: 수정

--- apps/client/src/shared/router/routes/protected-route.tsx
+++ apps/client/src/shared/router/routes/protected-route.tsx
@@ -1,19 +1,14 @@
 import { Navigate, Outlet } from 'react-router';
 
 import { authService } from '@shared/auth/services/auth-service.ts';
-import { tokenService } from '@shared/auth/services/token-service';
 
 import { routePath } from '../path';
 
 export function ProtectedRoute() {
   const isAuthenticated = authService.isAuthenticated();
-  const isTermsChecked = tokenService.getIsTermsToken() === 'true';
 
   if (!isAuthenticated) {
     return <Navigate to={routePath.SPLASH} replace />;
-  }
-  if (isTermsChecked) {
-    return <Navigate to={routePath.HOME} replace />;
   }
 
   return <Outlet />;
