ecf2499f4ffd1371b3aadc3b26d14cc1ecb7ebb0
hansoojeongsj <tnwjd3758@gmail.com>
2025-07-18 20:07:30 +0900

    fix: 로컬스토리지 검사 후 온보딩 못가게 막기

--- apps/client/src/shared/router/routes/protected-route.tsx
+++ apps/client/src/shared/router/routes/protected-route.tsx
@@ -1,14 +1,22 @@
-import { Navigate, Outlet } from 'react-router';
+import { Navigate, Outlet, useLocation } from 'react-router';
 
 import { authService } from '@shared/auth/services/auth-service.ts';
+import { tokenService } from '@shared/auth/services/token-service';
 
 import { routePath } from '../path';
 
 export function ProtectedRoute() {
   const isAuthenticated = authService.isAuthenticated();
+  const isTermsChecked = tokenService.getIsTermsToken() === 'true';
+  const location = useLocation();
 
   if (!isAuthenticated) {
     return <Navigate to={routePath.SPLASH} replace />;
+  }
+  const isOnOnboardingPage = location.pathname === routePath.ONBOARDING;
+
+  if (isTermsChecked && isOnOnboardingPage) {
+    return <Navigate to={routePath.HOME} replace />;
   }
 
   return <Outlet />;
