2fca67f1419c827447f9ae457b7f3ebe67716689
곽지욱 <99489686+gwagjiug@users.noreply.github.com>
2025-07-29 19:25:59 +0900

    Feat(client): 카카오 로그인 URL 및 리다이렉트 URI 로컬 & 배포 환경 설정 추가 (#354)
    feat: 카카오 로그인 URL 및 리다이렉트 URI 설정 추가

--- apps/client/src/shared/configs/app-config.ts
+++ apps/client/src/shared/configs/app-config.ts
@@ -1,21 +1,18 @@
-import { routePath } from '@shared/router/path.ts';
-
+import { routePath } from '@shared/router/path';
 /**
  * 애플리케이션 전체 설정을 관리하는 Config 파일
  */
 const DEFAULT_CONFIG = {
   auth: {
     isEnabled: true, // 인증 기능 활성화 여부
-
     loginSuccessUrl: routePath.LOGIN_FALLBACK,
     loginFailureUrl: routePath.LOGIN,
-
     kakaoLoginUrl: import.meta.env.VITE_KAKAO_LOGIN_URL || '',
+    kakaoLocalRedirectUrl: import.meta.env.VITE_KAKAO_LOCAL_REDIRECT_URI || '',
+    kakaoProdRedirectUrl: import.meta.env.VITE_KAKAO_PROD_REDIRECT_URI || '',
   },
-
   api: {
     baseUrl: import.meta.env.VITE_API_BASE_URL,
-    kakaoRedirectUrl: import.meta.env.VITE_KAKAO_REDIRECT_URI,
   },
 };
 
