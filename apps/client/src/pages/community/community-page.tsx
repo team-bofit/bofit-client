7e596c999d89703e90b1a87948928bdf310febd0
jeonghoon11 <jih361@naver.com>
2025-08-15 01:30:48 +0900

    refactor: useNavigateTo 훅을 사용하여 내비게이션 로직 간소화

--- apps/client/src/pages/community/community-page.tsx
+++ apps/client/src/pages/community/community-page.tsx
@@ -1,24 +1,17 @@
-import { useNavigate } from 'react-router';
-
 import { Navigation } from '@bds/ui';
 import { Icon } from '@bds/ui/icons';
 
 import CommunityBody from '@widgets/community/components/community-body/community-body';
 
+import { useNavigateTo } from '@shared/hooks/use-navigate-to';
 import { routePath } from '@shared/router/path';
 
 const CommunityPage = () => {
-  const navigate = useNavigate();
-
-  const handleNavigate = (path: string) => () => {
-    navigate(path);
-  };
-
   return (
     <>
       <Navigation
         rightIcon={<Icon name="home" />}
-        onClickRight={handleNavigate(routePath.HOME)}
+        onClickRight={useNavigateTo(routePath.HOME)}
         title="커뮤니티"
       />
 
