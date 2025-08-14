c00410cc5ec29517e479c2881c41d5a82533c6e2
jeonghoon11 <jih361@naver.com>
2025-08-14 18:21:51 +0900

    refactor: useNavigateTo 훅을 추가하여 내비게이션 로직 간소화

--- apps/client/src/pages/community/community-detail.tsx
+++ apps/client/src/pages/community/community-detail.tsx
@@ -1,38 +1,28 @@
 import { useParams } from 'react-router';
-import { useNavigate } from 'react-router-dom';
 
 import { Navigation } from '@bds/ui';
 import { Icon } from '@bds/ui/icons';
 
 import DetailSection from '@widgets/community/components/detail-section/detail-section';
 
+import { useNavigateTo } from '@shared/hooks/use-navigate-to';
 import { routePath } from '@shared/router/path';
 
 const CommunityDetail = () => {
-  const navigate = useNavigate();
-
   const { postId } = useParams<{ postId: string }>();
 
   if (!postId) {
     throw new Error('postId가 존재하지 않습니다.');
   }
 
-  const handleNavigate = (path: string | number) => {
-    if (typeof path === 'string') {
-      navigate(path);
-    } else {
-      navigate(path);
-    }
-  };
-
   return (
     <>
       <Navigation
         title="커뮤니티"
         leftIcon={<Icon name="caret_left_lg" width="2.4rem" height="2.4rem" />}
-        onClickLeft={() => handleNavigate(-1)}
+        onClickLeft={useNavigateTo(-1)}
         rightIcon={<Icon name="home" />}
-        onClickRight={() => handleNavigate(routePath.HOME)}
+        onClickRight={useNavigateTo(routePath.HOME)}
       />
 
       <DetailSection postId={postId} />
