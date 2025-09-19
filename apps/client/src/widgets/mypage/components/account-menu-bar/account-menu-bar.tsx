f69a3b625672e858b5fe0a3180808bb189974180
jeonghoon11 <jih361@naver.com>
2025-09-19 18:38:47 +0900

    feat: 카카오 로그아웃 기능 추가

--- apps/client/src/widgets/mypage/components/account-menu-bar/account-menu-bar.tsx
+++ apps/client/src/widgets/mypage/components/account-menu-bar/account-menu-bar.tsx
@@ -1,5 +1,7 @@
 import { TextButton } from '@bds/ui';
 
+import { useSocialLogout } from '../../hooks/use-social-logout';
+
 import * as styles from './account-menu-bar.css';
 
 const ACCOUNT_MENU_BAR_TEXT = {
@@ -8,14 +10,26 @@ const ACCOUNT_MENU_BAR_TEXT = {
 };
 
 const AccountMenuBar = () => {
+  const { kakaoLogout } = useSocialLogout();
+
+  const handleLogout = () => {
+    kakaoLogout();
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.leftButtonWrapper}>
-        <TextButton color="black" size="sm">
+        <TextButton
+          color="black"
+          size="sm"
+          onClick={() => {
+            // TODO: 회원탈퇴 기능 구현
+          }}
+        >
           {ACCOUNT_MENU_BAR_TEXT.WITHDRAW}
         </TextButton>
       </div>
-      <TextButton color="black" size="sm">
+      <TextButton color="black" size="sm" onClick={handleLogout}>
         {ACCOUNT_MENU_BAR_TEXT.LOGOUT}
       </TextButton>
     </div>
