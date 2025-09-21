45018cd44f81a2504592f3eac8b7340bbaca954d
곽지욱(Hanji) <99489686+gwagjiug@users.noreply.github.com>
2025-09-22 00:20:13 +0900

    Feat(client): 필터 드롭다운 컴포넌트 및 useToggle 훅 추가 (#412)
    * feat: 필터 드롭다운 컴포넌트 및 useToggle 훅 추가
    
    * feat: 필터 드롭다운 스타일 수정 및 useToggle 문서에서 불필요한 주석 제거

---
+++ apps/client/src/widgets/community/components/filter-dropdown/filter-dropdown.css.ts
@@ -0,0 +1,37 @@
+import { style } from '@vanilla-extract/css';
+
+import { themeVars } from '@bds/ui/styles';
+
+export const DropDownContainer = style({
+  position: 'relative',
+  display: 'inline-flex',
+  alignItems: 'flex-end',
+  flexDirection: 'column',
+});
+
+export const DropDownTitle = style({
+  ...themeVars.fontStyles.title_sb_16,
+  color: themeVars.color.gray800,
+  display: 'flex',
+  alignItems: 'center',
+  gap: '0.2rem',
+});
+
+export const DropDownIcon = style({
+  transition: 'transform 0.3s ease',
+  cursor: 'pointer',
+});
+
+export const DropDownContent = style({
+  position: 'absolute',
+  top: '3.2rem',
+  width: '9rem',
+  display: 'flex',
+  padding: '0.6rem 0',
+  flexDirection: 'column',
+  alignItems: 'center',
+  gap: '0.8rem',
+  borderRadius: '1.2rem',
+  border: `1px solid ${themeVars.color.gray200}`,
+  background: themeVars.color.white,
+});
