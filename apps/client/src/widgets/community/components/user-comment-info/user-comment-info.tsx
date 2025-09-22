6d8bc8ded0b8aef2e76d802abc3230de09d9db02
jeonghoon11 <jih361@naver.com>
2025-09-22 20:56:47 +0900

    feat: 사용자 댓글 정보 컴포넌트 분리

---
+++ apps/client/src/widgets/community/components/user-comment-info/user-comment-info.tsx
@@ -0,0 +1,69 @@
+import { Avatar, TextButton } from '@bds/ui';
+
+import { Image } from '@shared/types/type.ts';
+
+import * as styles from './user-comment-info.css';
+
+const DELETE_CONTENT = '삭제';
+
+interface UserCommentInfoProps {
+  content?: string;
+  writerNickName?: string;
+  createdAt?: string;
+  onClickDelete?: VoidFunction;
+  profileImage?: string;
+  isCommentOwner: boolean;
+  imageUrl: Image[];
+}
+
+const UserCommentInfo = ({
+  content,
+  writerNickName,
+  createdAt,
+  onClickDelete,
+  profileImage,
+  isCommentOwner,
+  imageUrl,
+}: UserCommentInfoProps) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.commentContainer}>
+        <div className={styles.userInfoContainer}>
+          <div className={styles.userInfo}>
+            <Avatar size="md" src={profileImage} />
+            <div>
+              <h2 className={styles.nickName}>{writerNickName}</h2>
+              <p className={styles.timestamp}>{createdAt}</p>
+            </div>
+          </div>
+          <div className={styles.button}>
+            {isCommentOwner ? (
+              <TextButton color="black" onClick={onClickDelete} size="sm">
+                {DELETE_CONTENT}
+              </TextButton>
+            ) : (
+              ''
+            )}
+          </div>
+        </div>
+        <p className={styles.comment}>{content}</p>
+      </div>
+      {imageUrl.length > 0 && (
+        <div className={styles.imageContainer}>
+          {imageUrl.map((image) =>
+            image.imageUrl && image.imageUrl.trim() !== '' ? (
+              <img
+                key={image.imageId}
+                className={styles.postImage}
+                src={image.imageUrl}
+                alt="post image"
+              />
+            ) : null,
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default UserCommentInfo;
