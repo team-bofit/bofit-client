5c69ee7e5d358dbabeccfca2a69271863c3356de
JeongHoon <113702672+jeonghoon11@users.noreply.github.com>
2025-09-23 22:34:44 +0900

    Feat(client): 마이 페이지 회원 탈퇴, 로그아웃 기능 구현 (#407)
    * refactor: 마이페이지 폴더 분리
    
    * feat: 마이페이지에 account-menu-bar 컴포넌트 추가
    
    * fix: 스키마 업데이트
    
    * feat: 카카오 로그아웃 기능 추가
    
    * feat: 카카오 로그아웃 리다이렉트 URL 설정 추가
    
    * fix: preview 컨테이너의 최대 높이 조정 (account-menu-bar 높이 만큼)
    
    * feat: 스키마 업데이트
    
    * feat: 카카오 회원 탈퇴 기능 추가
    
    * fix: 모달 콘텐츠의 폰트 크기를 16에서 14로 수정
    
    * feat: 로그아웃 및 탈퇴 모달 추가와 관련된 기능 구현
    
    * refactor: 로그아웃 및 탈퇴 모달의 핸들러명 변경과 상수화 제거
    
    * chore: 카카오 로그아웃 및 탈퇴 훅 파일 삭제
    
    * feat: 카카오 로그아웃 및 탈퇴 API 엔드포인트 추가
    
    * feat: 카카오 로그아웃 및 탈퇴 기능을 위한 쿼리 및 뮤테이션 옵션 추가
    
    * feat: 카카오 로그아웃 및 탈퇴 쿼리 키 추가
    
    * feat: 카카오 로그아웃 및 회원탈퇴 응답 타입 추가
    
    * fix: 탈퇴 모달의 콘텐츠 텍스트 형식 수정
    
    * feat: 카카오 로그아웃 및 탈퇴 기능 개선 - 성공 시 쿼리 무효화 및 리다이렉트 추가
    
    * feat: leftButtonWrapper를 horizontal-divider로 분리
    
    * feat: horizontalDivider를 verticalDivider로 이름 변경
    
    * feat: 사용자 섹션에 그림자 효과 추가 및 사용자 콘텐츠에서 제거
    
    * fix: 탈퇴 모달의 콘텐츠 텍스트 줄바꿈 수정 및 모달 컴포넌트 textStyle 복원
    
    * fix: 카카오 로그아웃 관련 설정에서 불필요한 리다이렉트 URL 제거
    
    * refactor: 카카오 로그아웃 및 탈퇴 관련 API 엔드포인트를 AUTH 섹션으로 이동

---
+++ apps/client/src/widgets/mypage/components/account-menu-bar/account-menu-bar.css.ts
@@ -0,0 +1,24 @@
+import { style } from '@vanilla-extract/css';
+
+import { themeVars } from '@bds/ui/styles';
+
+export const container = style({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  gap: '1rem',
+
+  position: 'sticky',
+  bottom: 0,
+  width: '100%',
+  height: '5.8rem',
+
+  padding: '1rem',
+  backgroundColor: themeVars.color.white,
+});
+
+export const verticalDivider = style({
+  width: '0.1rem',
+  height: '2.5rem',
+  backgroundColor: themeVars.color.gray500,
+});
