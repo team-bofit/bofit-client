7fa9ba70cb795bcdc6dd46dae7bde61a9e60358c
jogpfls <joerin4177@gmail.com>
2025-09-25 03:28:56 +0900

    feat: api 구현 전 임시 mock 데이터

---
+++ apps/client/src/widgets/onboarding/mocks/etc-mocks.ts
@@ -0,0 +1,14 @@
+export const etcData = {
+  renewalTypes: [{ displayName: '갱신형' }, { displayName: '비갱신형' }],
+  refuntType: [
+    { displayName: '순수보장형' },
+    { displayName: '일부환급형' },
+    { displayName: '만기환급형' },
+  ],
+  paymentPeriodYears: [
+    { displayName: '10년' },
+    { displayName: '20년' },
+    { displayName: '30년' },
+  ],
+  maturityAge: [{ displayName: '80세' }, { displayName: '100세' }],
+};
