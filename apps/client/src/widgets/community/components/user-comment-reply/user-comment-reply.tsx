8fe88a299ef6f8074666dfd72118528d5dd170a1
jeonghoon11 <jih361@naver.com>
2025-09-24 09:47:34 +0900

    feat: 대댓글 컴포넌트 추가 및 스타일 정의

---
+++ apps/client/src/widgets/community/components/user-comment-reply/user-comment-reply.tsx
@@ -0,0 +1,78 @@
+import { Avatar, TextButton } from '@bds/ui';
+import { Icon } from '@bds/ui/icons';
+
+import FilterDropDown from '@widgets/community/components/filter-dropdown/filter-dropdown';
+
+import { Image } from '@shared/types/type';
+
+import * as styles from './user-comment-reply.css';
+
+interface UserCommentReplyProps {
+  profileImage: string;
+  writerNickName: string;
+  createdAt: string;
+  content: string;
+  images: Image[];
+}
+
+const UserCommentReply = ({
+  profileImage,
+  writerNickName,
+  createdAt,
+  content,
+  images,
+}: UserCommentReplyProps) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.userInfoContainer}>
+        <div className={styles.leftContainer}>
+          <Icon name="recomment_line" width="2rem" height="2rem" />
+          <div className={styles.userInfo}>
+            <Avatar size="sm" src={profileImage} />
+            <div>
+              <p className={styles.nickName}>{writerNickName}</p>
+              <p className={styles.createdAt}>{createdAt}</p>
+            </div>
+          </div>
+        </div>
+        <div className={styles.iconButtonContainer}>
+          <FilterDropDown IconType="more">
+            <TextButton
+              size="sm"
+              color="black"
+              onClick={() => {
+                // @TODO: 댓글 수정 API 연동
+              }}
+            >
+              수정
+            </TextButton>
+            <TextButton
+              size="sm"
+              color="error"
+              onClick={() => {
+                // @TODO: 댓글 삭제 API 연동
+              }}
+            >
+              삭제
+            </TextButton>
+          </FilterDropDown>
+        </div>
+      </div>
+      <div className={styles.commentContainer}>
+        <p className={styles.comment}>{content}</p>
+      </div>
+      <div className={styles.imageContainer}>
+        {images.map((image: Image) => (
+          <img
+            className={styles.replyImage}
+            key={image.imageId}
+            src={image.imageUrl}
+            alt="comment reply image"
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default UserCommentReply;
