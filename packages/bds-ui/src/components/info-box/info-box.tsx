6b6a4b45f3334de6d84d5af740f35c4c8bddbe4b
jogpfls <joerin4177@gmail.com>
2025-09-25 03:51:54 +0900

    feat: info-box에 하이라이트가 있는 설명일 때 베리에이션 추가

--- packages/bds-ui/src/components/info-box/info-box.tsx
+++ packages/bds-ui/src/components/info-box/info-box.tsx
@@ -1,3 +1,5 @@
+import { ReactNode } from 'react';
+
 import { Icon } from '@bds/ui/icons';
 
 import * as styles from './info-box.css';
@@ -6,15 +8,23 @@ interface InfoBoxProps {
   description?: string;
   size: 'md' | 'sm';
   iconSize: string;
+  highlightDescription?: ReactNode;
 }
 
-const InfoBox = ({ size, description, iconSize }: InfoBoxProps) => {
+const InfoBox = ({
+  size,
+  description,
+  iconSize,
+  highlightDescription,
+}: InfoBoxProps) => {
   return (
     <div className={styles.additionalContainer({ size })}>
       <div className={styles.iconContainer}>
         <Icon name="bulb" size={iconSize} color="primary500" />
       </div>
-      <dd className={styles.contents({ size })}>{description}</dd>
+      <p className={styles.contents({ size })}>
+        {highlightDescription ?? description}
+      </p>
     </div>
   );
 };
