73485701d4a4caa461210d986b9ff0110b5f57f9
jogpfls <joerin4177@gmail.com>
2025-09-29 01:24:13 +0900

    feat: 유효성 검사 error 시 다음으로 버튼 비활성화

--- apps/client/src/pages/onboarding/onboarding-page.tsx
+++ apps/client/src/pages/onboarding/onboarding-page.tsx
@@ -44,7 +44,12 @@ const OnboardingPage = () => {
     mode: 'onChange',
     defaultValues: onboardingDefaultValues,
   });
-  const { watch, getValues, handleSubmit } = methods;
+  const {
+    watch,
+    getValues,
+    handleSubmit,
+    formState: { errors },
+  } = methods;
 
   const { openModal, closeModal } = useModal();
   const navigate = useNavigate();
@@ -79,7 +84,7 @@ const OnboardingPage = () => {
     switch (currentStep) {
       case 'user': {
         const { name, gender, job } = watch();
-        return !!name && !!gender && !!job;
+        return !!name && !!gender && !!job && !errors.name;
       }
       case 'health': {
         const health = watch('health');
