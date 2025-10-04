b1d9d09ad77f7bfd96be3716e14465b588d481f0
gwagjiug <kwjo0228@naver.com>
2025-10-04 20:00:34 +0900

    refactor: 프로필 이미지 추가 버튼 및 편집 기능 구현

--- apps/client/src/widgets/mypage/components/body/body.tsx
+++ apps/client/src/widgets/mypage/components/body/body.tsx
@@ -1,7 +1,10 @@
-import { Avatar, Button } from '@bds/ui';
+import { Avatar, Button, Input } from '@bds/ui';
+import { Icon } from '@bds/ui/icons';
 
 import AccountMenuBar from '@widgets/mypage/components/account-menu-bar/account-menu-bar';
 import Preview from '@widgets/mypage/components/preview/preview';
+
+import { useToggle } from '@shared/hooks/use-toggle';
 
 import * as styles from './body.css';
 
@@ -11,14 +14,30 @@ interface ContentProps {
 }
 
 const Body = ({ nickname, profileImage }: ContentProps) => {
+  const [isEditing, toggleEditing] = useToggle(false);
+
+  const handleProfileEdit = () => {
+    toggleEditing();
+  };
+
   return (
     <section className={styles.userSection}>
       <div className={styles.userContent}>
-        <Avatar size={'lg'} src={profileImage} />
+        <div className={styles.userProfileSection}>
+          <Avatar size={'lg'} src={profileImage} />
+          <div className={styles.addImageContainer}>
+            <Icon
+              name="img_add"
+              width={'2rem'}
+              height={'2rem'}
+              color="gray900"
+            />
+          </div>
+        </div>
         <div className={styles.contentName}>
           {nickname}
-          <Button variant="white_fill" size="lg">
-            프로필 편집
+          <Button variant="white_fill" size="lg" onClick={handleProfileEdit}>
+            {isEditing ? '프로필 편집 완료하기' : '프로필 편집'}
           </Button>
         </div>
       </div>
