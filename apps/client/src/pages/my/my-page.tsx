f4308a8b4825f25fdca287ac7b39b20a220dddc8
jogpfls <joerin4177@gmail.com>
2025-10-06 03:06:05 +0900

    feat: navigationBar에 search 추가

--- apps/client/src/pages/my/my-page.tsx
+++ apps/client/src/pages/my/my-page.tsx
@@ -7,6 +7,7 @@ import { Icon } from '@bds/ui/icons';
 import Body from '@widgets/mypage/components/body/body';
 
 import { USER_QUERY_OPTIONS } from '@shared/api/domain/mypage/queries';
+import { useNavigateTo } from '@shared/hooks/use-navigate-to';
 import { routePath } from '@shared/router/path';
 
 const MyPage = () => {
@@ -28,6 +29,8 @@ const MyPage = () => {
       <Navigation
         title={`${userData?.nickname}님 반가워요!`}
         rightIcon={<Icon name="home" color="white" />}
+        searchIcon={<Icon name="search" color="white" />}
+        onClickSearch={useNavigateTo(routePath.COMMUNITY_SEARCH)}
         onClickRight={() => handleNavigate(routePath.HOME)}
         backgroundColor="primary"
         textColor="white"
