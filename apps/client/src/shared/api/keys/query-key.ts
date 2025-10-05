7ff29d27d72112a78649ab3c3c9667c91bf2a81f
조혜린 <164613169+jogpfls@users.noreply.github.com>
2025-10-05 23:53:24 +0900

    Feat(client): 커뮤니티 필터링 및 게시글 정렬 구현 (#437)
    * feat: 커뮤니티 페이지 드롭다운 적용
    
    * feat: 리팩토링 된 Chip 반영 전 임시 Chip 컴포넌트 적용
    
    * feat: all 카테고리 추가
    
    * feat: 카테고리 필터링 api 연결
    
    * fix: 필수값이 아니도록 수정
    
    * chore: 상수 파일로 분리
    
    * refactor: feedList의 컴포넌트 정렬 및 카테고리 처리 로직 단순화
    
    * fix: onClick 수정
    
    * fix: 스타일 수정
    
    * feat: chip 컴포넌트 적용
    
    * feat: chip 컴포넌트 round large에 cursor 추가
    
    * fix: 스크롤 css 스타일 수정
    
    * fix: 잘못된 분기 수정

--- apps/client/src/shared/api/keys/query-key.ts
+++ apps/client/src/shared/api/keys/query-key.ts
@@ -24,7 +24,12 @@ export const USER_QUERY_KEY = {
 
 export const COMMUNITY_QUERY_KEY = {
   ALL: ['community'],
-  FEED_PREVIEW: () => [...COMMUNITY_QUERY_KEY.ALL, 'feed'],
+  FEED_PREVIEW: (sort?: string, category?: string) => [
+    ...COMMUNITY_QUERY_KEY.ALL,
+    'feed',
+    sort,
+    category,
+  ],
   FEED_DETAIL: (postId: string) => [
     ...COMMUNITY_QUERY_KEY.ALL,
     'detail',
@@ -46,7 +51,10 @@ export const COMMUNITY_QUERY_KEY = {
 
 export const COMMUNITY_MUTATION_KEY = {
   POST_COMMENT: () => [...COMMUNITY_QUERY_KEY.COMMENTS(), 'create'],
-  POST_FEED: () => [...COMMUNITY_QUERY_KEY.FEED_PREVIEW(), 'create'],
+  POST_FEED: (sort?: string, category?: string) => [
+    ...COMMUNITY_QUERY_KEY.FEED_PREVIEW(sort, category),
+    'create',
+  ],
   PUT_FEED: (postId: string) => [
     ...COMMUNITY_QUERY_KEY.FEED_DETAIL(postId),
     'update',
---
+++ apps/client/src/widgets/community/constant/category.ts
@@ -0,0 +1,6 @@
+export const CATEGORIES = [
+  { value: 'ALL', label: '전체' },
+  { value: 'QNA', label: '보험 QnA' },
+  { value: 'INFORMATION', label: '정보공유' },
+  { value: 'CONVERSATION', label: '사담' },
+] as const;
