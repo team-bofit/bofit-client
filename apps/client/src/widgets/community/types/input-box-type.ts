884097d4067831d48118343fde5c645b6bc65fb9
jeonghoon11 <jih361@naver.com>
2025-10-06 01:23:34 +0900

    feat: 커뮤니티 상세 페이지 inputBox 입력 모드 리듀서 및 타입 정의 추가

---
+++ apps/client/src/widgets/community/types/input-box-type.ts
@@ -0,0 +1,23 @@
+export type InputBoxMode =
+  | { type: 'comment'; action: 'create'; postId: string }
+  | {
+      type: 'comment';
+      action: 'edit';
+      postId: string;
+      commentId: number;
+      initialContent: string;
+    }
+  | { type: 'reply'; action: 'create'; postId: string; parentCommentId: number }
+  | {
+      type: 'reply';
+      action: 'edit';
+      postId: string;
+      commentId: number;
+      initialContent: string;
+    };
+
+export type ReducerAction =
+  | { type: 'COMMENT_EDIT'; commentId: number; initialContent: string }
+  | { type: 'REPLY_CREATE'; parentCommentId: number }
+  | { type: 'REPLY_EDIT'; commentId: number; initialContent: string }
+  | { type: 'RESET' };
