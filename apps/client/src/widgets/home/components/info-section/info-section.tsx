84f9104094db3a079eea576e38330a2a154032ce
minjeoong <orange_mj@naver.com>
2025-10-06 22:27:36 +0900

    fix: 보험 추천 받지 않은 유저 - home 페이지 캐러셀 2건 변경

--- apps/client/src/widgets/home/components/info-section/info-section.tsx
+++ apps/client/src/widgets/home/components/info-section/info-section.tsx
@@ -1,8 +1,7 @@
 import { useNavigate } from 'react-router-dom';
-import { Autoplay } from 'swiper/modules';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { SwiperSlide } from 'swiper/react';
 
-import { Button } from '@bds/ui';
+import { Button, Carousel } from '@bds/ui';
 
 import HomeCard from '@widgets/home/components/home-card/home-card.tsx';
 import { homeCardConfig } from '@widgets/home/configs/home-card-config.ts';
@@ -34,38 +33,22 @@ export const InfoSection = () => {
           딱 맞는 보험, 어렵지 않게 찾을 수 있어요!
         </p>
       </div>
-      <div className={styles.homeCardList}>
-        <Swiper
-          spaceBetween={8}
-          slidesPerView="auto"
-          loop={true}
-          autoplay={{
-            delay: 0,
-            disableOnInteraction: false,
-            pauseOnMouseEnter: true,
-          }}
-          speed={1500}
-          modules={[Autoplay]}
-          allowTouchMove={true}
-          centeredSlides={true}
-          className={styles.homeCardList}
-        >
-          {homeCardConfig.map((card, index) => (
-            <SwiperSlide key={index} style={{ width: 'auto' }}>
-              <HomeCard
-                icon={
-                  <img
-                    src={card.icon}
-                    alt={card.target}
-                    className={styles.homeCardIcon}
-                  />
-                }
-                title={card.target}
-              />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
+      <Carousel slidesPerView={4.5} autoPlay className={styles.homeCardList}>
+        {homeCardConfig.map((card, index) => (
+          <SwiperSlide key={index} style={{ width: 'auto' }}>
+            <HomeCard
+              icon={
+                <img
+                  src={card.icon}
+                  alt={card.target}
+                  className={styles.homeCardIcon}
+                />
+              }
+              title={card.target}
+            />
+          </SwiperSlide>
+        ))}
+      </Carousel>
       <div className={styles.bottomButton}>
         <Button variant={'white_fill'} size={'lg'} onClick={handleNavigate}>
           맞춤 보험 추천 받으러 가기
