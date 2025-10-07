89184fcd7665815c43055df599fe6300656129fc
jeonghoon11 <jih361@naver.com>
2025-10-08 05:27:36 +0900

    fix: 캐러셀 오른쪽 container padding 수정

--- apps/client/src/widgets/community/components/live-popular-feed/live-popular-feed.css.ts
+++ apps/client/src/widgets/community/components/live-popular-feed/live-popular-feed.css.ts
@@ -3,7 +3,7 @@ import { style } from '@vanilla-extract/css';
 export const container = style({
   display: 'flex',
   flexDirection: 'column',
-  padding: '1.8rem 1.9rem 2.4rem',
+  padding: '1.8rem 0 2.4rem 1.9rem ',
 });
 
 export const carousel = style({
