99f1fb73936a3f7359a76df7a5e42a6c6d49fe09
JeongHoon <113702672+jeonghoon11@users.noreply.github.com>
2025-10-08 05:30:43 +0900

    Feat(client): 실시간 인기 게시글 컴포넌트 구현 (#435)
    * feat: 실시간 인기 게시글 내부 card 컴포넌트 구현
    
    * feat: 실시간 인기 게시글 컴포넌트 및 스타일 추가
    
    * feat: 커뮤니티 미리보기 컴포넌트에 실시간 인기 게시글 추가
    
    * feat: 목 데이터 파일 분리
    
    * feat: 주석 제거
    
    * fix: 실시간 인기 게시물 아이콘 변경
    
    * fix: 라이브 인기 피드의 패딩 값 수정
    
    * fix: Carousel 컴포넌트 적용
    
    * feat: LivePopularFeed 컴포넌트 추가 및 FeedList 구조 개선
    
    * feat: 캐러셀 높이 지정
    
    * fix: 좋아요, 댓글 수 위치 변경
    
    * fix: 캐러셀 개당 잡히지 않는 문제 해결
    
    * fix: borderRadius px로 수정
    
    * fix: 캐러셀 오른쪽 container padding 수정
    
    ---------
    
    Co-authored-by: minjeoong <orange_mj@naver.com>

---
+++ apps/client/src/widgets/community/components/live-popular-feed/live-popular-feed.css.ts
@@ -0,0 +1,27 @@
+import { style } from '@vanilla-extract/css';
+
+export const container = style({
+  display: 'flex',
+  flexDirection: 'column',
+  padding: '1.8rem 0 2.4rem 1.9rem ',
+});
+
+export const carousel = style({
+  height: '12.9rem',
+});
+
+export const carouselItem = style({
+  width: '24.7rem',
+});
+
+export const titleContainer = style({
+  display: 'inline-flex',
+  gap: '0.2rem',
+  marginBottom: '0.8rem',
+});
+
+export const indicatorWrapper = style({
+  display: 'flex',
+  justifyContent: 'center',
+  marginTop: '1.2rem',
+});
