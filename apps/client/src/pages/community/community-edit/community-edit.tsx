322f8654b41c4b75700d5775ff663696bf0309ae
곽지욱(Hanji) <99489686+gwagjiug@users.noreply.github.com>
2025-10-13 20:29:06 +0900

    Feat(client): 커뮤니티 글 수정  (#473)
    * fix: 커뮤니티 편집 페이지에서 상수 제거 및 텍스트 직접 사용으로 수정
    
    * fix: community-edit 페이지에서 카테고리 초기화 및 유효성 검사 로직 개선
    
    * fix: user-detail-meta 컴포넌트에서 버튼 텍스트를 상수 대신 직접 사용으로 수정
    
    * fix: community-edit 페이지에서 상태 관리 개선 및 카테고리 선택 로직 수정
    
    * fix: 커뮤니티 편집 페이지에서 이미지 업로드 및 삭제 기능 추가
    
    * fix: 커뮤니티 편집 페이지에서 이미지 업로드 로직 개선 및 상태 관리 수정
    
    * fix: 커뮤니티 편집 페이지에서 이미지 시퀀스 계산 로직 수정

--- apps/client/src/pages/community/community-edit/community-edit.tsx
+++ apps/client/src/pages/community/community-edit/community-edit.tsx
@@ -1,17 +1,30 @@
 import { ChangeEvent, useState } from 'react';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import {
+  useMutation,
+  useQueryClient,
+  useSuspenseQuery,
+} from '@tanstack/react-query';
+import { useNavigate, useParams } from 'react-router-dom';
 
 import { Input, Navigation, TextButton, Title } from '@bds/ui';
 import { Icon } from '@bds/ui/icons';
 
+import CommunityImageUploader from '@widgets/community/components/community-image-uploader/community-image-uploader';
 import CommunityLine from '@widgets/community/components/community-line/community-line';
 import FilterDropDown from '@widgets/community/components/filter-dropdown/filter-dropdown';
 import { categoryOptions } from '@widgets/community/configs/category-config';
 import { PLACEHOLDER } from '@widgets/community/constant/input-placeholder';
 import { CategoryType } from '@widgets/community/types/category-type';
+import { isValidImage } from '@widgets/community/utils/type-guard';
 
-import { COMMUNITY_MUTATION_OPTIONS } from '@shared/api/domain/community/queries';
+import {
+  COMMUNITY_MUTATION_OPTIONS,
+  COMMUNITY_QUERY_OPTIONS,
+} from '@shared/api/domain/community/queries';
+import {
+  MUTATION_QUERY_OPTIONS,
+  uploadImageToS3,
+} from '@shared/api/domain/queries';
 import { COMMUNITY_QUERY_KEY } from '@shared/api/keys/query-key';
 import {
   LIMIT_LONG_TEXT,
@@ -19,44 +32,22 @@ import {
 } from '@shared/constants/text-limits';
 import { useLimitedInput } from '@shared/hooks/use-limited-input';
 import { routePath } from '@shared/router/path';
+import { extractS3Urls } from '@shared/utils/utils';
 
 import * as styles from './community-edit.css';
-
-const COMMUNITY_CONTENT = {
-  TITLE: {
-    HEADER: '제목',
-    BODY: '내용',
-  },
-  BUTTON: '완료',
-};
 
 const CommunityEdit = () => {
   const navigate = useNavigate();
   const queryClient = useQueryClient();
   const { postId } = useParams<{ postId: string }>();
-  const location = useLocation();
-  const state = location.state as {
-    title: string;
-    content: string;
-    category?: { category: string; description: string };
-  };
-  const [title, setTitle] = useState(state.title);
-  const [content, setContent] = useState(state.content);
-  const getInitialCategory = () => {
-    if (!state.category) {
-      return null;
-    }
-    const matchedOption = categoryOptions.find(
-      (option) => option.value === state.category?.category,
-    );
-    return matchedOption ?? null;
-  };
-  const [category, setCategory] = useState(getInitialCategory);
-  const { isErrorState } = useLimitedInput(LIMIT_SHORT_TEXT, title.length);
 
   if (!postId) {
     throw new Error('게시글 Id가 존재하지 않습니다.');
   }
+
+  const { data: feedDetailData } = useSuspenseQuery(
+    COMMUNITY_QUERY_OPTIONS.FEED_DETAIL(postId),
+  );
 
   const { mutate, isPending } = useMutation({
     ...COMMUNITY_MUTATION_OPTIONS.PUT_FEED(postId),
@@ -68,28 +59,80 @@ const CommunityEdit = () => {
     },
   });
 
-  const handlePutFeed = () => {
-    if (isDisabled || !category) {
-      return;
+  const { mutate: postImageUploadMutate } = useMutation({
+    ...MUTATION_QUERY_OPTIONS.POST_IMAGE(),
+  });
+
+  if (!feedDetailData) {
+    throw new Error(
+      '글 수정 페이지에서 원본 피드 데이터를 불러오지 못했습니다.',
+    );
+  }
+
+  const [title, setTitle] = useState(feedDetailData?.title || '');
+  const [content, setContent] = useState(feedDetailData?.content || '');
+  const [category, setCategory] = useState(
+    feedDetailData?.category?.category || '',
+  );
+  const [newImages, setNewImages] = useState<
+    { file: File; previewUrl: string }[]
+  >([]);
+  const [deletedImageIds, setDeletedImageIds] = useState<number[]>([]);
+  const [updatedImages, setUpdatedImages] = useState<
+    { id?: number; imageUrl: string; sequence: number }[]
+  >(
+    () =>
+      feedDetailData.imageUrl?.filter(isValidImage).map((img, index) => ({
+        id: img.imageId,
+        imageUrl: img.imageUrl,
+        sequence: index + 1,
+      })) ?? [],
+  );
+  const { isErrorState } = useLimitedInput(LIMIT_SHORT_TEXT, title.length);
+
+  const handleUploadNewImages = async () => {
+    if (newImages.length === 0) {
+      return [];
     }
-    //@TODO 타입 에러로 임시 작성해둠. 추후 구현 시 수정 필요
+
+    const data = await new Promise<{ presignedUrls: string[] }>(
+      (resolve, reject) =>
+        postImageUploadMutate(
+          newImages.map((item) => item.file.type),
+          { onSuccess: resolve, onError: reject },
+        ),
+    );
+
+    await Promise.all(
+      data.presignedUrls.map((url, idx) =>
+        uploadImageToS3(url, newImages[idx].file),
+      ),
+    );
+
+    const uploadedUrls = extractS3Urls(data.presignedUrls).map((url, idx) => ({
+      imageUrl: url,
+      sequence: updatedImages.length + idx,
+    }));
+
+    setUpdatedImages((prev) => [...prev, ...uploadedUrls]);
+    setNewImages([]);
+
+    return uploadedUrls;
+  };
+
+  const handlePutFeed = async () => {
+    const newUploadedImages = await handleUploadNewImages();
+
     mutate({
       body: {
         newTitle: title,
         newContent: content,
-        newCategory: category.value,
-        deleteImageIds: [],
-        updatedImages: [],
+        newCategory: category,
+        deleteImageIds: deletedImageIds,
+        updatedImages: [...updatedImages, ...newUploadedImages],
       },
     });
   };
-
-  const isTitleValid = title.trim().length > 0;
-  const isContentValid = content.trim().length > 0;
-  const isCategoryValid = Boolean(category?.value);
-
-  const isDisabled =
-    !(isTitleValid && isContentValid && isCategoryValid) || isPending;
 
   const handleGoBack = () => {
     navigate(-1);
@@ -108,7 +151,25 @@ const CommunityEdit = () => {
   };
 
   const handleCategory = (option: CategoryType) => {
-    setCategory(option);
+    setCategory(option.value);
+  };
+
+  const handleImageChange = (files: FileList) => {
+    const newImages = Array.from(files).map((file) => ({
+      file,
+      previewUrl: URL.createObjectURL(file),
+    }));
+    setNewImages((prev) => [...prev, ...newImages]);
+  };
+
+  const handleRemoveNewImage = (urlToRemove: string) => {
+    setNewImages((prev) =>
+      prev.filter((item) => item.previewUrl !== urlToRemove),
+    );
+  };
+
+  const handleRemoveOriginImage = (imageId: number) => {
+    setDeletedImageIds((prev) => [...prev, imageId]);
   };
 
   return (
@@ -127,30 +188,34 @@ const CommunityEdit = () => {
           <TextButton
             size="sm"
             color="primary"
-            disabled={isDisabled}
+            disabled={
+              isPending || !title.trim() || !content.trim() || !category
+            }
             onClick={() => {
-              (handlePutFeed(), handleGoBack());
+              handlePutFeed();
             }}
           >
-            {COMMUNITY_CONTENT.BUTTON}
+            완료
           </TextButton>
         }
-        isTextButton={true}
+        isTextButton
       />
       <div className={styles.postContainer}>
         <div className={styles.postHeader}>
           <div className={styles.postTitle}>
-            <Title fontStyle="eb_md">{COMMUNITY_CONTENT.TITLE.HEADER}</Title>
+            <Title fontStyle="eb_md">제목</Title>
             <FilterDropDown
-              optionTitle={category ? category.label : '카테고리 선택'}
-              rightIcon={<Icon name="caret_up_sm" />}
+              optionTitle={
+                categoryOptions.find((opt) => opt.value === category)?.label
+              }
+              rightIcon={<Icon name="caret_down_sm" />}
               isIconRotate
             >
               {categoryOptions.map((option) => (
                 <TextButton
-                  key={option.value}
+                  key={option.label}
                   size="sm"
-                  color={category?.value === option.value ? 'primary' : 'black'}
+                  color={category === option.value ? 'primary' : 'black'}
                   onClick={() => handleCategory(option)}
                 >
                   {option.label}
@@ -167,10 +232,51 @@ const CommunityEdit = () => {
           />
         </div>
         <div className={styles.postContent}>
-          <Title fontStyle="eb_md">{COMMUNITY_CONTENT.TITLE.BODY}</Title>
+          <Title fontStyle="eb_md">내용</Title>
           <CommunityLine value={content} onChange={handleContentChange} />
+          {(feedDetailData.imageUrl?.some(isValidImage) ||
+            newImages.length > 0) && (
+            <div className={styles.imageContainer}>
+              {feedDetailData.imageUrl
+                ?.filter(isValidImage)
+                .filter((img) => !deletedImageIds.includes(img.imageId))
+                .map((orgImage) => (
+                  <div key={orgImage.imageId} className={styles.imageItem}>
+                    <img
+                      className={styles.postImage}
+                      src={orgImage.imageUrl}
+                      alt="uploaded"
+                    />
+                    <TextButton
+                      color="black"
+                      size="sm"
+                      onClick={() => handleRemoveOriginImage(orgImage.imageId)}
+                    >
+                      삭제
+                    </TextButton>
+                  </div>
+                ))}
+              {newImages.map((image) => (
+                <div key={image.previewUrl} className={styles.imageItem}>
+                  <img
+                    className={styles.postImage}
+                    src={image.previewUrl}
+                    alt="preview"
+                  />
+                  <TextButton
+                    color="black"
+                    size="sm"
+                    onClick={() => handleRemoveNewImage(image.previewUrl)}
+                  >
+                    삭제
+                  </TextButton>
+                </div>
+              ))}
+            </div>
+          )}
         </div>
       </div>
+      <CommunityImageUploader onChange={handleImageChange} />
     </div>
   );
 };
