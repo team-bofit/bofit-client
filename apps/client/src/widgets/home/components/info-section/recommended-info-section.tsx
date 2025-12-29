e6f6257e017710abc6eb559d94f4c2497ea84474
minjeoong <orange_mj@naver.com>
2025-12-30 01:27:07 +0900

    fix: build 오류 해결

--- apps/client/src/widgets/home/components/info-section/recommended-info-section.tsx
+++ apps/client/src/widgets/home/components/info-section/recommended-info-section.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useMemo } from 'react';
+import { useCallback } from 'react';
 import { useSuspenseQuery } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
 
